b1d1cf2c0b98a09f350b46fa1c7ea21f3d9c3826
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2024-08-07 21:33:02 +0900

    [D3] Title: 부먹왕국의 차원 관문, Time: 839 ms, Memory: 125,056 KB -BaekjoonHub

--- SWEA/D3/7964. 부먹왕국의 차원 관문/부먹왕국의 차원 관문.java
+++ SWEA/D3/7964. 부먹왕국의 차원 관문/부먹왕국의 차원 관문.java
@@ -1,46 +1,31 @@
 
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Solution {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        int tt = 1;
-
-        while (tt <= t) {
-            StringTokenizer st = new StringTokenizer(br.readLine().trim());
-            int city = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
-
-            st = new StringTokenizer(br.readLine().trim());
-            StringBuilder s = new StringBuilder("1");
-            for (int i = 0; i < city; i++) {
-                s.append(st.nextToken());
-            }
-            s.append("1");
-
-            int cnt = 0;
-            int dnt = 0;
-
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '1') {
-                    dnt = 0;
-                } else {
-                    dnt++;
-                    if (dnt == d) {
-                        cnt++;
-                        dnt = 0;
-                    }
-                }
-            }
-
-            System.out.println("#" + tt + " " + cnt);
-            tt++;
-        }
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int tt = 1;
+		while (tt <= t) {
+			int city = sc.nextInt();
+			int d = sc.nextInt();
+			int cnt = 0;
+			int dnt = 0;
+			for (int i = 0; i < city; i++) {
+				if (sc.nextInt() == 1) {
+					dnt = 0;
+				} else {
+					dnt++;
+					if (dnt == d) {
+						cnt++;
+						dnt = 0;
+					}
+				}
+			}
+			System.out.println("#" + tt + " " + cnt);
+			tt++;
+		}
+	}
 }
