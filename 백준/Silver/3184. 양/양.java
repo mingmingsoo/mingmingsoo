3d9b5a86d59db79c36db878fa55e4acb373717cc
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2024-08-21 21:00:53 +0900

    [Silver I] Title: 양, Time: 256 ms, Memory: 22312 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.java
+++ 백준/Silver/3184. 양/양.java
@@ -1,79 +1,84 @@
 
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
+	
 	static int n;
 	static int m;
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
+		
 		n = sc.nextInt();
 		m = sc.nextInt();
-
+		
 		char[][] grid = new char[n][m];
-
-		for (int i = 0; i < n; i++) {
-			String line = sc.next();
-			for (int j = 0; j < m; j++) {
-				grid[i][j] = line.charAt(j);
+		
+		for(int i = 0; i<n; i++) {
+			String s = sc.next();
+			for(int j = 0; j<m;j++) {
+				grid[i][j] = s.charAt(j);
 			}
-
 		}
-		int ans_o = 0;
-		int ans_v = 0;
+		
+		// v이거나 o면 dfs 탐색 -> 탐색했으면 #으로 바꿔주기
+		// # 만나면 나가기
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (grid[i][j]=='o'||grid[i][j]=='v') {
-					O_cnt = 0;
-					V_cnt = 0;
+		int o_total = 0;
+		int v_total = 0;
+		for(int i = 0; i<n; i++) {
+			for(int j = 0; j<m;j++) {
+				if(grid[i][j]=='v'||grid[i][j]=='o') {
+					o_cnt = 0;
+					v_cnt = 0;
 					dfs(grid, i, j);
-					if(O_cnt>V_cnt) {
-						ans_o+= O_cnt;
+					if(o_cnt>v_cnt) {
+						v_cnt = 0;
 					}
 					else {
-						ans_v +=V_cnt;
+						o_cnt = 0;
 					}
+					o_total += o_cnt;
+					v_total += v_cnt;
 				}
 			}
 		}
-		System.out.println(ans_o+" "+ans_v);
+		System.out.println(o_total+" "+v_total);
+
 
 	}
-	static int O_cnt;
-	static int V_cnt;
+	static int o_cnt;
+	static int v_cnt;
 
 	private static void dfs(char[][] grid, int i, int j) {
-		int[] row = { 1, -1, 0, 0 };
-		int[] col = { 0, 0, 1, -1 };
 		
 		if(grid[i][j]=='#') {
 			return;
 		}
-
+		
 		
 		if(grid[i][j]=='o') {
-			O_cnt++;
+			o_cnt++;
 		}
 		else if(grid[i][j]=='v') {
-			V_cnt++;
+			v_cnt++;
 		}
 		
-		grid[i][j] = '#'; // 방문 처리
+		grid[i][j] = '#'; //방문 처리
+		
+		int[] row = {1,-1,0,0};
+		int[] col = {0,0,1,-1};
 		
 		for(int k = 0; k<4;k++) {
 			int nr = i+row[k];
 			int nc = j+col[k];
-			if(nr>=0&&nr<n&&nc>=0&&nc<m) dfs(grid,nr,nc);
+			
+			if(nr>=0&& nr<n&&nc>=0&& nc<m) {
+				dfs(grid, nr, nc);
+			}
+			
 		}
-		
-		
-
 		
 	}
 
