28547b4af53d3a2958d8fae6b863fc69aa08157f
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2024-09-02 08:43:35 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,048 ms, Memory: 23,460 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,5 +1,4 @@
 
-
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -8,42 +7,38 @@ public class Solution {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
 		int t = sc.nextInt();
-		int tt = 1;
-		while (tt <= t) {
+		int tt =1;
+		while(tt<=t) {
 			a = new int[9];
 			b = new int[9];
-			boolean[] bool = new boolean[19];
-
 			visited = new boolean[9];
-
+			boolean[] bool = new boolean[19];
 			for (int i = 0; i < 9; i++) {
 				a[i] = sc.nextInt();
 				bool[a[i]] = true;
 			}
-			int idx = 0;
+			int bidx = 0;
 			for (int i = 1; i < 19; i++) {
 				if (!bool[i]) {
-					b[idx++] = i;
+					b[bidx++] = i;
 				}
 			}
 			wincount = 0;
 			losecount = 0;
-
-			game(0, 0, 0);
-			System.out.println("#" + tt + " " + wincount + " " + losecount);
+			game(0, 0, 0); // idx awin bwin
+			System.out.println("#"+tt+" "+wincount + " " + losecount);
 			tt++;
-
 		}
 	}
 
-	static boolean visited[];
+	static int wincount;
+	static int losecount;
+	static boolean[] visited;
 	static int[] a;
 	static int[] b;
 
 	private static void game(int idx, int atotal, int btotal) {
-
 		if (idx == 9) {
 			if (atotal > btotal) {
 				wincount++;
@@ -54,19 +49,19 @@ public class Solution {
 		}
 
 		for (int i = 0; i < 9; i++) {
-			if (!visited[i]) {
-				visited[i] = true;
+			if(!visited[i]) {
 				if (a[idx] > b[i]) {
+					visited[i] = true;
 					game(idx + 1, atotal + a[idx] + b[i], btotal);
-				} else {
+					visited[i] = false;
+				} else if (a[idx] < b[i]) {
+					visited[i] = true;
 					game(idx + 1, atotal, btotal + a[idx] + b[i]);
+					visited[i] = false;
 				}
-				visited[i] = false;
 			}
 		}
-	}
 
-	static int wincount;
-	static int losecount;
+	}
 
 }
