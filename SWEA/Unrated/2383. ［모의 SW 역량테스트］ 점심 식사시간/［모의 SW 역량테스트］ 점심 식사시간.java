9dce375f294bf0978dbe3ca6352c31c846405dd5
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2024-09-09 10:20:40 +0900

    [Unrated] Title: [모의 SW 역량테스트] 점심 식사시간, Time: 235 ms, Memory: 37,236 KB -BaekjoonHub

--- SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
+++ SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
@@ -11,128 +11,107 @@ public class Solution {
 
 		Scanner sc = new Scanner(System.in);
 
-		long b = System.currentTimeMillis();
-
 		int t = sc.nextInt();
 		int tt = 1;
-		while (tt <= t) {
 
+		while (tt <= t) {
+			int n = sc.nextInt();
+			int[][] grid = new int[n][n];
 			personlist = new ArrayList<>();
-			stairs = new Stair[2];
-			n = sc.nextInt();
-			grid = new int[n][n];
+			stairlist = new Stair[2];
 			int idx = 0;
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
 					grid[i][j] = sc.nextInt();
 					if (grid[i][j] == 1) {
 						personlist.add(new Person(i, j));
-					}
-					if (grid[i][j] > 1) {
-						stairs[idx++] = new Stair(i, j, grid[i][j]);
+					} else if (grid[i][j] > 1) {
+						stairlist[idx++] = new Stair(i, j, grid[i][j]);
 					}
 				}
 			}
-//			System.out.println(personlist);
-//			System.out.println(Arrays.toString(stairs));
+//		System.out.println(personlist);
+//		System.out.println(Arrays.toString(stairlist));
 
 			ans = Integer.MAX_VALUE;
 			subset(0);
 			System.out.println("#" + tt + " " + ans);
 			tt++;
-
 		}
-
-		long a = System.currentTimeMillis();
-		long Time = (a - b);
-//		System.out.println("시간차이(m) : " + Time);
 
 	}
 
-	static int n;
-	static int[][] grid;
-	static int perIdx;
 	static int ans;
 	static List<Person> personlist;
-	static Stair[] stairs;
-	static int cnt;
+	static Stair[] stairlist;
 
 	private static void subset(int idx) {
-		if (cnt > Math.pow(2, personlist.size() - 1)) {
-			return;
-		}
-		if (idx == personlist.size()) { // 6명
-			int totaltime = 0;
-			// 각 계단마다 사람 이동시간 구할거임.
-			for (int i = 0; i < 2; i++) { // 0번 계단 -> 1번 계단 순으로.
 
-				// 계단이 가까운 순으로 넣을 거임.
+		if (idx == personlist.size()) {
+			int totaltime = 0;
+			// 1. 각 계단마다, 이동시간을 구해줄거임!
+			for (int i = 0; i < 2; i++) {
 				PriorityQueue<Person> pq = new PriorityQueue<>();
+				// 2-1. 거리가 짧은 애부터 q에 넣어주기.
 				for (int j = 0; j < personlist.size(); j++) {
-					if (personlist.get(j).stair == i) {
+					if (personlist.get(j).stair == i) { // 0번 계단인 애들만!
 						pq.add(personlist.get(j));
 					}
 				}
-//				System.out.println(personlist);
-
-				// 가까운 순서부터 계단 내려가는 시간 계산.
-				int[] timearr = new int[100]; // 매 분마다 사람 수 저장.
+				// 2-2. 가까운 순서부터 계단 내려가는 시간 계산
+				int[] timecount = new int[100];
 				while (!pq.isEmpty()) {
 					Person front = pq.poll();
-					int start = front.moveTime; // 계단까지 이동 거리
-					int end = start + stairs[i].len; // 계단 다 내려가는 시간.
+					int start = front.movetime; // 계단까지 이동거리
+					int end = start + stairlist[i].len; // 계단 다 내려가는 시간.
 					for (int j = start; j < end; j++) {
-						if (timearr[j] == 3) {
+						if (timecount[j] == 3) {
 							end++;
 							continue;
 						} else {
-							timearr[j]++;
+							timecount[j]++;
 						}
 					}
-					totaltime = Math.max(totaltime, end); // 최종 시간은 end의 마지막 값. 0과 1의 end값 중 큰 게 최종 시간임.
 
+					totaltime = Math.max(totaltime, end);
 				}
 
 			}
-			ans = Math.min(totaltime, ans);
+			ans = Math.min(ans, totaltime);
 			return;
 		}
 
-		// 0번 계단 선택
-		personlist.get(idx).stair = 0; // 0 번 계단 선택. // sel[idx] = 0 // 과 동일/.
-		personlist.get(idx).moveTime = 1 + Math.abs(personlist.get(idx).x - stairs[0].x)
-				+ Math.abs(personlist.get(idx).y - stairs[0].y);
+		// 0번 계단일 때
+		personlist.get(idx).stair = 0;
+		personlist.get(idx).movetime = 1 + Math.abs(stairlist[0].x - personlist.get(idx).x)
+				+ Math.abs(stairlist[0].y - personlist.get(idx).y);
 		subset(idx + 1);
 
-		// 1번 계단 선택
+		// 1번 계단일 때
 		personlist.get(idx).stair = 1;
-		personlist.get(idx).moveTime = 1 + Math.abs(personlist.get(idx).x - stairs[1].x)
-				+ Math.abs(personlist.get(idx).y - stairs[1].y);
+		personlist.get(idx).movetime = 1 + Math.abs(stairlist[1].x - personlist.get(idx).x)
+				+ Math.abs(stairlist[1].y - personlist.get(idx).y);
 		subset(idx + 1);
+
 	}
 
 	static class Person implements Comparable<Person> {
 		int x;
 		int y;
-		int moveTime; // 계단과의 거리
-		int stair; // 선택한 계단
+		int movetime;
+		int stair;
 
 		public Person(int x, int y) {
 			super();
 			this.x = x;
 			this.y = y;
-
-		}
-
-		@Override
-		public String toString() {
-			return "Person [x=" + x + ", y=" + y + ", moveTime=" + moveTime + ", stair=" + stair + "]";
 		}
 
 		@Override
 		public int compareTo(Person o) {
-			return Integer.compare(this.moveTime, o.moveTime);
+			return Integer.compare(this.movetime, o.movetime);
 		}
+
 	}
 
 	static class Stair {
@@ -146,11 +125,5 @@ public class Solution {
 			this.y = y;
 			this.len = len;
 		}
-
-		@Override
-		public String toString() {
-			return "Stair [x=" + x + ", y=" + y + ", len=" + len + "]";
-		}
-
 	}
 }
