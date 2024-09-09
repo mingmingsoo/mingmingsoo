99cf169da79ea61043fc2f0cd0f621d2b9c540b0
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2024-09-09 15:05:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 861 ms, Memory: 95,736 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -5,100 +5,94 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
+		// 최대 쏠 수 있는 횟수는 k
+		// 즉 k 번쏘면 무조건 통과는 하게 되어있음
+		// 위에서부터 한줄씩 쏠꺼임.
 
 		Scanner sc = new Scanner(System.in);
 
 		int t = sc.nextInt();
 		int tt = 1;
+
 		while (tt <= t) {
-			n = sc.nextInt();
-			m = sc.nextInt();
+			d = sc.nextInt();
+			w = sc.nextInt();
 			k = sc.nextInt();
 
-			grid = new int[n][m];
-
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
+			grid = new int[d][w];
+			for (int i = 0; i < d; i++) {
+				for (int j = 0; j < w; j++) {
 					grid[i][j] = sc.nextInt();
 				}
 			}
-			ans = k; // k번이 ans의 최댓값임! k번 쏘면 무조건 통과니까!
-			change(0, 0); // r , 쏜 횟수
 
-//		매번 각 행은 3가지 경우의 수가 나온다 -> 안쐈을 때. 0 쐈을 때. 1 쐈을 때
-			// 조건을 통과하는 최소 shoot 수를 구하기.
+			ans = Integer.MAX_VALUE;
+			dfs(0, 0); // 0: 행 0: 쏜 횟수
 			System.out.println("#" + tt + " " + ans);
 			tt++;
 		}
 
 	}
 
-	static int n;
-	static int m;
-	static int k;
-	static int[][] grid;
 	static int ans;
+	static int k;
+	static int d;
+	static int w;
+	static int[][] grid;
 
-	private static void change(int r, int shoot) {
-		if (pass(0)) {
-			ans = Math.min(shoot, ans);
+	private static void dfs(int r, int cnt) {
+		if (pass()) {
+			if (ans > cnt) {
+				ans = cnt;
+			}
 			return;
 		}
 
-		if (r == n) {
+		if (cnt >= k) {
 			return;
 		}
 
-		if (shoot >= ans) {
+		if (r == d) {
 			return;
 		}
 
-		// 원본 배열 생성
+		// 안 쏠때
+		dfs(r + 1, cnt);
+
+		// 쏠 때
 		int[] tmp = grid[r].clone();
 
-		// 그냥 넘어갈 때
-		change(r + 1, shoot);
-
-		// 0을 쐈을 때
+		// 0 쐈음
 		Arrays.fill(grid[r], 0);
-		change(r + 1, shoot + 1);
+		dfs(r + 1, cnt + 1);
 
-		// 1을 쐈을 때
+		// 1 쐈음
 		Arrays.fill(grid[r], 1);
-		change(r + 1, shoot + 1);
+		dfs(r + 1, cnt + 1);
 
-		// 원상 복구
 		grid[r] = tmp;
 
 	}
 
-	private static boolean pass(int c) {
+	private static boolean pass() {
 
-		if (c == m) {
-			return true;
+		con: for (int j = 0; j < w; j++) {
+			int cnt = 1;
+			for (int i = 0; i < d - 1; i++) {
+				if (grid[i][j] == grid[i + 1][j]) {
+					cnt++;
+				} else {
+					cnt = 1;
+				}
+				if (cnt >= k) {
+					continue con;
+				}
+			}
+			return false;
 		}
 
-		int r = 0;
-		int cnt = 0;
-		int cur = grid[r][c];
+		return true;
 
-		while (r < n - 1) {
-			if (cur == grid[r + 1][c]) {
-				cnt++;
-				cur = grid[r + 1][c];
-				r++;
-			} else {
-				r++;
-				cnt = 0;
-				cur = grid[r][c];
-			}
-
-			if (cnt == k - 1) {
-				return pass(c + 1);
-			}
-		}
-
-		return false;
 	}
 
 }
