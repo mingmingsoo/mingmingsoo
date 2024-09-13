1e3118c0c2696b8162e141005b86ad455447c135
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2024-09-13 14:59:55 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 192 ms, Memory: 18060 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,4 +1,5 @@
 
+
 import java.util.Scanner;
 
 public class Main {
@@ -7,120 +8,65 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		n = sc.nextInt();
-		grid = new int[n][n];
-		visited = new boolean[n][n];
+		int n = sc.nextInt();
 
-		int[] tmprow = { 0, 0, 1, -1 };
-		int[] tmpcol = { 1, -1, 0, 0 };
+		int[][] grid = new int[n][n];
 
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				grid[i][j] = sc.nextInt();
-				if (grid[i][j] == 1) {
-					visited[i][j] = true; // 벽 못가게 true 처리.
+			}
+		}
+
+		// dp 를 사용해서...
+		// 0 : 가로, 1: 세로, 2: 대각선
+		int[][][] dp = new int[n][n][3];
+		dp[0][1][0] = 1;
+
+		for (int j = 2; j < n; j++) {
+			if (grid[0][j] == 0) { // 첫 줄은 가로로 밖에 못감!
+				dp[0][j][0] = dp[0][j - 1][0];
+			}
+		}
+
+		for (int i = 1; i < n; i++) {
+			for (int j = 2; j < n; j++) {
+				if (grid[i][j] != 0) {
+					continue;
 				}
+
+				dp[i][j][0] += dp[i][j - 1][0] + dp[i][j - 1][2]; // 가로로 올 수 있는 경우의 수는 가로 + 대각선
+				dp[i][j][1] += dp[i - 1][j][1] + dp[i - 1][j][2]; // 세로로 올 수 있는 경우의 수는 세로 + 대각선
+
+				if (grid[i - 1][j] == 0 && grid[i][j - 1] == 0) {
+					dp[i][j][2] += dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+				} // 대각선으로 올 수 있는 경우의 수는 가 + 세 + 대
+
 			}
 		}
-
-		dfs(0, 0, 0, 1);
-		System.out.println(cnt);
-
-	}
-
-	static int n;
-	static int[][] grid;
-	static boolean[][] visited;
-	static int[] row = { 0, 1, 1 };
-	static int[] col = { 1, 1, 0 };
-	static int cnt;
-
-	private static void dfs(int r1, int c1, int r2, int c2) {
-		// 위치가 가로일 때는 방향 0, 1 만 가능
-		// 위치가 세로일 때는 방향 1, 2 만 가능
-		// 위치가 대각선 일때는 방향 0, 1, 2 가능
-
-		if (r2 == n - 1 && c2 == n - 1) { // 마지막 지점에 도달했으면 경우의 수 +1
-			cnt++;
-			return;
-		}
-
-		visited[r1][c1] = true;
-		visited[r2][c2] = true;
-
-		// 가로로 위치할 때
-		if (r1 == r2 && c2 - c1 == 1) {
-			for (int k = 0; k < 2; k++) {
-				int nr1 = r2;
-				int nc1 = c2;
-				int nr2 = r2 + row[k];
-				int nc2 = c2 + col[k];
-
-				if (nr2 >= 0 && nr2 < n && nc2 >= 0 && nc2 < n && !visited[nr2][nc2]) {
-					if (k == 1 && (grid[nr2 - 1][nc2] == 1 || grid[nr2][nc2 - 1] == 1)) {
-						continue;
-					}
-					visited[r2][c2] = true;
-					dfs(nr1, nc1, nr2, nc2);
-				}
-			}
-		}
-
-		// 세로로 위치할 때
-		if (r2 - r1 == 1 && c1 == c2) {
-			for (int k = 1; k < 3; k++) {
-				int nr1 = r2;
-				int nc1 = c2;
-				int nr2 = r2 + row[k];
-				int nc2 = c2 + col[k];
-
-				if (nr2 >= 0 && nr2 < n && nc2 >= 0 && nc2 < n && !visited[nr2][nc2]) {
-					if (k == 1 && (grid[nr2 - 1][nc2] == 1 || grid[nr2][nc2 - 1] == 1)) {
-						continue;
-					}
-					visited[r2][c2] = true;
-					dfs(nr1, nc1, nr2, nc2);
-				}
-			}
-		}
-
-		// 대각선으로 위치할 때
-		if (r2 - r1 == 1 && c2 - c1 == 1) {
-			for (int k = 0; k < 3; k++) {
-				int nr1 = r2;
-				int nc1 = c2;
-				int nr2 = r2 + row[k];
-				int nc2 = c2 + col[k];
-
-				if (nr2 >= 0 && nr2 < n && nc2 >= 0 && nc2 < n && !visited[nr2][nc2]) {
-					if (k == 1 && (grid[nr2 - 1][nc2] == 1 || grid[nr2][nc2 - 1] == 1)) {
-						continue;
-					}
-					visited[r2][c2] = true;
-					dfs(nr1, nc1, nr2, nc2);
-				}
-			}
-
-		}
-
-		visited[r1][c1] = false;
-		visited[r2][c2] = false;
-
-	}
-
-	private static void check(int r2, int c2, int k) {
-		int nr1 = r2;
-		int nc1 = c2;
-		int nr2 = r2 + row[k];
-		int nc2 = c2 + col[k];
-
-		if (nr2 >= 0 && nr2 < n && nc2 >= 0 && nc2 < n && !visited[nr2][nc2]) {
-			if (k == 1 && (grid[nr2 - 1][nc2] == 1 || grid[nr2][nc2 - 1] == 1)) {
-				return;
-			}
-			visited[r2][c2] = true;
-			dfs(nr1, nc1, nr2, nc2);
-		}
+//		for (int i = 0; i < n; i++) {
+//			for (int j = 0; j < n; j++) {
+//				System.out.print(dp[i][j][0] + " ");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+//		for (int i = 0; i < n; i++) {
+//			for (int j = 0; j < n; j++) {
+//				System.out.print(dp[i][j][1] + " ");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+//		for (int i = 0; i < n; i++) {
+//			for (int j = 0; j < n; j++) {
+//				System.out.print(dp[i][j][2] + " ");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+		
+		System.out.println(dp[n-1][n-1][0]+dp[n-1][n-1][1]+dp[n-1][n-1][2]);
 
 	}
 
