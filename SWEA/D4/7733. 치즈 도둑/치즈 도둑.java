a63a5a918cd0d1dfd43e6b0901a2034cae25e8bc
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2024-09-28 15:23:31 +0900

    [D4] Title: 치즈 도둑, Time: 475 ms, Memory: 100,956 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -1,6 +1,6 @@
 
-
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Solution {
@@ -10,49 +10,44 @@ public class Solution {
 		Scanner sc = new Scanner(System.in);
 		
 		int t = sc.nextInt();
+		
 		int tt = 1;
+		
 		while(tt<=t) {
-			
 			n = sc.nextInt();
 			grid = new int[n][n];
-			visited = new boolean[n][n];
 
-			int min = 101;
 			int max = 0;
-
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
-					int tmp = sc.nextInt();
-					grid[i][j] = tmp;
-					if (tmp > max) {
-						max = tmp;
-					}
-					if (tmp < min) {
-						min = tmp;
+					grid[i][j] = sc.nextInt();
+					if (grid[i][j] > max) {
+						max = grid[i][j];
 					}
 				}
 			}
-
 			int ans = 1;
-			for (int k = min; k < max; k++) {
+			int day = 1;
+			while (day < max) {
 				visited = new boolean[n][n];
 				for (int i = 0; i < n; i++) {
 					for (int j = 0; j < n; j++) {
-						if (grid[i][j] <= k) {
-							visited[i][j] = true;
+						if (grid[i][j] <= day) {
+							grid[i][j] = 0; // 먹힘처리
 						}
 					}
 				}
 				int cnt = 0;
 				for (int i = 0; i < n; i++) {
 					for (int j = 0; j < n; j++) {
-						if (!visited[i][j]) {
-							dfs(i, j);
+						if (!visited[i][j] && grid[i][j] != 0) {
+							bfs(i, j);
 							cnt++;
 						}
 					}
 				}
 				ans = Math.max(ans, cnt);
+				day++;
 			}
 			System.out.println("#"+tt+" "+ans);
 			tt++;
@@ -61,24 +56,32 @@ public class Solution {
 
 	}
 
-	static int[][] grid;
-	static boolean[][] visited;
-	static int n;
-	static int[] row = { 1, -1, 0, 0 };
+	static int[] row = { -1, 1, 0, 0 };
 	static int[] col = { 0, 0, 1, -1 };
 
-	private static void dfs(int r, int c) {
+	private static void bfs(int r, int c) {
 		visited[r][c] = true;
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] { r, c });
 
-		for (int k = 0; k < 4; k++) {
-			int nr = r + row[k];
-			int nc = c + col[k];
-			if (nr >= 0 && nr < n && nc >= 0 && nc < n && !visited[nr][nc]) {
-				dfs(nr, nc);
-				visited[nr][nc] = true;
+		while (!q.isEmpty()) {
+			int[] node = q.poll();
+			int rr = node[0];
+			int cc = node[1];
+			for (int k = 0; k < 4; k++) {
+				int nr = rr + row[k];
+				int nc = cc + col[k];
+				if (nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] != 0 && !visited[nr][nc]) {
+					visited[nr][nc] = true;
+					q.add(new int[] { nr, nc });
+				}
 			}
 		}
 
 	}
+
+	static boolean visited[][];
+	static int grid[][];
+	static int n;
 
 }
