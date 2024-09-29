2e5aa91c4913e5048866f9f81585624bd0f0a6f1
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2024-09-29 17:21:12 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 275 ms, Memory: 23,656 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,3 +1,5 @@
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
@@ -5,52 +7,44 @@ public class Solution {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
 		int t = sc.nextInt();
+		
 		int tt = 1;
-		while (tt <= t) {
-			fee = new int[4]; // 요금
-			days = new int[12]; // 이용 계획
+		
+		while(tt<=t) {
+			
+			int fees[] = new int[4];
+			int plans[] = new int[13];
 
 			for (int i = 0; i < 4; i++) {
-				fee[i] = sc.nextInt();
+				fees[i] = sc.nextInt();
+			}
+			for (int i = 1; i < 13; i++) {
+				plans[i] = sc.nextInt();
 			}
 
-			for (int i = 0; i < 12; i++) {
-				days[i] = sc.nextInt();
-			}
+			int[] dp = new int[13];
 
-			ans = fee[3]; // 일년치 젤 큰 값
+			for (int i = 1; i < 13; i++) {
+				// 일일권일때
+				dp[i] = dp[i - 1] + fees[0] * plans[i];
 
-			for (int i = 0; i < 12; i++) {
-				if (days[i] != 0) {
-					dfs(i + 1, 0); // 시작하는 달에 dfs! -> 연산이 조금이나마 줄으라고...
-					break;
+				// 한달
+				dp[i] = Math.min(dp[i], dp[i - 1] + fees[1]);// 예를들어 일일권 이용금액 vs 한달치 이용금액
+
+				// 세달
+				if (i >= 3) {
+					dp[i] = Math.min(dp[i], dp[i - 3] + fees[2]); // 3달치 이용금액이 적으면 3달치 pick
 				}
+
+				// 1년
+				if (i == 12)
+					dp[i] = Math.min(dp[i], fees[3]);
 			}
-			System.out.println("#" + tt + " " + ans);
+			System.out.println("#"+tt+" "+dp[12]);
 			tt++;
 		}
 
-	}
-
-	static int[] days;
-	static int[] fee;
-	static int ans;
-
-	private static void dfs(int month, int cost) {
-		if (month > 12) {
-			if (ans > cost) {
-				ans = cost;
-			}
-			return;
-		}
-		// 1일 일때
-		dfs(month + 1, cost + days[month - 1] * fee[0]);
-		// 1달 일때
-		dfs(month + 1, cost + fee[1]);
-		// 3달 일때
-		dfs(month + 3, cost + fee[2]);
 
 	}
 
