23b2e92d97d2f5c30ef04a170278d0e0ac03a4d9
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2024-12-29 15:35:41 +0900

    [Gold V] Title: 평범한 배낭, Time: 176 ms, Memory: 53840 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,41 +1,38 @@
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
 
-		int n = sc.nextInt(); // 물건 갯수
-
-		int k = sc.nextInt(); // 가방 최대 용량
-
-		int[] weight = new int[n + 1];
-		int[] cost = new int[n + 1];
-
-		for (int i = 1; i < n + 1; i++) {
-			weight[i] = sc.nextInt();
-			cost[i] = sc.nextInt();
-		}
-
-		int[][] dp = new int[n + 1][k + 1];
-
-		for (int i = 1; i < n + 1; i++) {
-			for (int j = 0; j < k + 1; j++) {
-				if (weight[i] <= j) {
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + cost[i]);
-				} else {
-					dp[i][j] = dp[i - 1][j];
-				}
-
-			}
-
-		}
-		System.out.println(dp[n][k]);
-
-	}
-
+        int[] weights = new int[N+1];
+        int[] costs = new int[N+1];
+        int[][] dp = new int[N+1][W+1];
+        for (int i = 1; i < N+1; i++) {
+            st = new StringTokenizer(br.readLine());
+            weights[i] = Integer.parseInt(st.nextToken());
+            costs[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i < N+1; i++) {
+            for (int w = 1; w < W+1; w++) {
+                if(weights[i]<=w){
+                    dp[i][w] = Math.max(dp[i-1][w], dp[i-1][w-weights[i]]+costs[i]);
+                }
+                else{
+                    dp[i][w] = dp[i-1][w];
+                }
+            }
+        }
+        System.out.println(dp[N][W]);
+    }
 }
