c6435a4b18891c5c597a8fabe1f2aa03ef359713
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2024-12-29 15:47:19 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18236 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,32 +1,38 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
+/**
+ * 3으로 나누어 떨어지면 3으로 나눔
+ * 2로 나누어 떨어지면 2로 나눔
+ * 1을 뺌
+ *
+ * 위 3가지 연산을  통해 1로 만든다.
+ * 연산 횟수 최솟값
+ */
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-		int n = sc.nextInt();
-		int[] dp = new int[n + 1];
+        int[] dp = new int[n+1];
+        for (int i = 2; i <=n ; i++) {
+            int minCnt = n;
+            minCnt = Math.min(dp[i-1]+1,minCnt);
+            if(i%2==0){
+                minCnt = Math.min(dp[i/2]+1,minCnt);
+            }
+            if(i%3==0){
+                minCnt = Math.min(dp[i/3]+1,minCnt);
+            }
+            dp[i] = minCnt;
+        }
 
-		for (int i = 2; i <= n; i++) {
-
-			dp[i] = dp[i - 1]+1;
-
-			if (i % 2 == 0) {
-				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-			}
-
-			if (i % 3 == 0) {
-				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-			}
-
-		}
-//		System.out.println(Arrays.toString(dp));
-		System.out.println(dp[n]);
-
-	}
-
+//        System.out.println(Arrays.toString(dp));
+        System.out.println(dp[n]);
+    }
 }
