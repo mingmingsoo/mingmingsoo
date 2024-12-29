59b316bdccac6cdd7eb7bb2093e74a7b682ba3a5
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2024-12-29 15:55:20 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,25 +1,29 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
+/**
+ * 2*n 크기 직사각형을 채우는 방법
+ * 9일 떄 55 이므로... 피보나치~
+ */
 public class Main {
 
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-
-		int n = sc.nextInt()+1;
-		
-		int[] dp = new int[n + 1];
-		dp[1] = 1;
-		dp[2] = 1;
-		for (int i = 2; i < n + 1; i++) {
-			dp[i] = (dp[i - 1] + dp[i - 2])%10007;
-
-		}
-//		System.out.println(Arrays.toString(dp));
-		System.out.println(dp[n]);
-		
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine())+1;
+        int[] dp = new int[n+1];
+        /*
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+         */
+        dp[1] = 1;
+        for (int i = 2; i <= n ; i++) {
+            dp[i] = (dp[i-1]+dp[i-2])%10007;
+        }
+//        System.out.println(Arrays.toString(dp));
+        System.out.println(dp[n]);
+    }
 }
