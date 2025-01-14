a59f5d0553afb6cb609e2add30c38c7f986b1bfc
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-01-14 16:12:50 +0900

    [Bronze V] Title: 사분면 고르기, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,28 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	// Main
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int x = Integer.parseInt(br.readLine());
+		int y = Integer.parseInt(br.readLine());
+		if (x > 0 && y > 0) {
+			System.out.println(1);
+			return;
+		} else if (x < 0 && y > 0) {
+			System.out.println(2);
+			return;
+		} else if (x < 0 && y < 0) {
+			System.out.println(3);
+			return;
+		}
+		System.out.println(4);
+
+	}
+}
