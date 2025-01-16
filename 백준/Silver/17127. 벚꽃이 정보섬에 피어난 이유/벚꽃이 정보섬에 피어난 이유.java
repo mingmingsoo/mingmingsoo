cb782ffbef4c39592210f7b09607ffc738687cd9
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-01-16 20:17:06 +0900

    [Silver V] Title: 벚꽃이 정보섬에 피어난 이유, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/17127. 벚꽃이 정보섬에 피어난 이유/벚꽃이 정보섬에 피어난 이유.java
+++ 백준/Silver/17127. 벚꽃이 정보섬에 피어난 이유/벚꽃이 정보섬에 피어난 이유.java
@@ -7,6 +7,8 @@ import java.util.StringTokenizer;
 
 public class Main {
 	// Main
+	static int[] arr;
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,7 +16,7 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 
 		// 그룹은 무조건 4개
-		int[] arr = new int[N];
+		arr = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
@@ -23,35 +25,27 @@ public class Main {
 		int ans = 0;
 
 		for (int i = 1; i < N; i++) { // 끝 점 설정.
-			int one = 1;
-			for (int x = 0; x < i; x++) {
-				one *= arr[x];
-			}
 			for (int j = i + 1; j < N; j++) {
-				int two = 1;
-				for (int x = i; x < j; x++) {
-					two *= arr[x];
-				}
 				for (int k = j + 1; k < N; k++) {
-					int three = 1;
-					for (int x = j; x < k; x++) {
-						three *= arr[x];
-					}
-					int four = 1;
-					for (int x = k; x < N; x++) {
-						four *= arr[x];
-					}
-//					System.out.println(i + " " + j + " " + k + " ");
-//					System.out.println(one + " " + two + " " + three + " " + four + " ");
-//					System.out.println("---------------------------");
+					int one = cal(0, i);
+					int two = cal(i, j);
+					int three = cal(j, k);
+					int four = cal(k, N);
 					ans = Math.max(ans, one + two + three + four);
 
 				}
 			}
 		}
-
 		System.out.println(ans);
 
+	}
+
+	private static int cal(int k, int n) {
+		int mul = 1;
+		for (int i = k; i < n; i++) {
+			mul *= arr[i];
+		}
+		return mul;
 	}
 
 }
