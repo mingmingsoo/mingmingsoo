c13a80a8fd39f929745d84652b739bd8d3d596ac
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-01-16 17:14:14 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 100 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,39 +1,33 @@
 
-import java.util.Arrays;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int t = sc.nextInt();
-		int tt =1;
-		
-		while(tt<=t) {
-			int k = sc.nextInt();
-			int n = sc.nextInt();
-			
-			int[][] grid = new int[k+1][n+1];
-			for(int j = 0; j<n+1;j++) {
-				grid[0][j] = j;
+	// Main
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int t = 0; t < T; t++) {
+			int k = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+			int[][] dp = new int[k + 1][n + 1];
+			for (int j = 1; j <= n; j++) {
+				dp[0][j] = j;
 			}
-			
-			for(int i = 1; i<k+1;i++) {
-				for(int j =0; j<n+1;j++) {
-					for(int w =0; w<=j;w++) {
-						grid[i][j] += grid[i-1][w];
-					}
-					
+
+			for (int i = 1; i <= k; i++) {
+				for (int j = 1; j <= n; j++) {
+					dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
 				}
 			}
-			
-//		System.out.println(Arrays.deepToString(grid));
-			System.out.println(grid[k][n]);
-			tt++;
+			sb.append(dp[k][n]).append("\n");
 		}
-		
-	}
+		System.out.println(sb);
 
+	}
 }
