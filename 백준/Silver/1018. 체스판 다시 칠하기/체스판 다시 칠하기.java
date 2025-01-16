9079e35c6b82e07c588987b1fab0b4590ca7ca4a
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-01-16 20:55:05 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -6,100 +6,52 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/**
- * 시작점이 W이거나 B이거나
- */
 public class Main {
+	// Main
+	public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new char[N][M];
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = str.charAt(j);
+			}
+		}
 
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        grid = new Character[n][m];
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i <= N - 8; i++) {
+			for (int j = 0; j <= M - 8; j++) {
+				min = Math.min(min, whatIsFirst('W', i, j));
+				min = Math.min(min, whatIsFirst('B', i, j));
+			}
+		}
+		System.out.println(min);
 
-        for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < m; j++) {
-                grid[i][j] = str.charAt(j);
-            }
-        }
-        int startI;
-        int startJ;
-        int ansW = Integer.MAX_VALUE;
-        int ansB = Integer.MAX_VALUE;
-        for (int i = 0; i <= n-8; i++) {
-            for (int j = 0; j <= m-8; j++) {
-                startI = i;
-                startJ = j;
-                int tmpW = changeW(startI, startJ);
-                int tmpB = changeB(startI, startJ);
-                if(tmpW<ansW){
-                    ansW = tmpW;
-                }
-                if(tmpB<ansB){
-                    ansB = tmpB;
-                }
-            }
-        }
+	}
 
-//        System.out.println(ansW);
-//        System.out.println(ansB);
-        if(ansW > ansB){
-            System.out.println(ansB);
-        }
-        else{
-            System.out.println(ansW);
-        }
-    }
+	static char[][] map;
+	static int N;
+	static int M;
 
-    private static int changeB(int startI, int startJ) {
-        int cnt = 0;
+	private static int whatIsFirst(char color, int r, int c) {
+		int change = 0;
+		for (int i = r; i < r + 8; i++) {
+			for (int j = c; j < c + 8; j++) {
+				if ((i + j) % 2 == 0 && map[i][j] != color) {
+					change++;
+				}
+				if ((i + j) % 2 == 1 && map[i][j] == color) {
+					change++;
+				}
+			}
+		}
 
-        for (int i = startI; i < startI+8; i++) {
-            for (int j = startJ; j < startJ+8; j++) {
-                if((i+j)%2==0&& grid[i][j]=='W'){
-                    cnt++;
-                }
-                else if((i+j)%2==1&&grid[i][j]=='B'){
-                    cnt++;
-                }
+		return change;
+	}
 
-            }
-        }
-
-
-
-
-
-        return cnt;
-    }
-
-    static int n;
-    static int m;
-    static Character[][] grid;
-
-    private static int changeW(int startI, int startJ) {
-        int cnt = 0;
-
-        for (int i = startI; i < startI+8; i++) {
-            for (int j = startJ; j < startJ+8; j++) {
-                if((i+j)%2==0&& grid[i][j]=='B'){
-                    cnt++;
-                }
-                else if((i+j)%2==1&&grid[i][j]=='W'){
-                    cnt++;
-                }
-
-            }
-        }
-
-
-
-
-        return cnt;
-    }
 }
