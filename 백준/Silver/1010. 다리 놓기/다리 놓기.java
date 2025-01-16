ad919c5d7ff86e7f71d9e8126faf9b628cf5a3d4
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-01-16 16:48:38 +0900

    [Silver V] Title: 다리 놓기, Time: 108 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,10 +1,9 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+// 머 이런 어이없는 문제가.
 public class Main {
 	// Main
 	public static void main(String[] args) throws IOException {
@@ -13,30 +12,23 @@ public class Main {
 
 		int T = Integer.parseInt(br.readLine());
 		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 		for (int t = 0; t < T; t++) {
 			st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
 
 			// mCn 구해야함
-			double mok = 1;
+			double ans = 1;
 			for (int i = M; i > (M - N); i--) {
-				mok *= i;
+				ans *= i;
 			}
-			double divide = 1;
 			for (int i = 1; i <= N; i++) {
-				divide *= i;
+				ans /= i;
 			}
-//			System.out.println(mok);
-//			System.out.println(divide);
-			double ans = mok/divide;
-			System.out.println(Math.round(ans));
-//			1
-//			20 21
-			
-//			1
-//			24 29
+			sb.append(Math.round(ans)).append("\n");
 		}
+		System.out.println(sb);
 
 	}
 }
