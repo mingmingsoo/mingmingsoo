973f803df2f74fa7bd9c6b2a4c5a2d9d46cd724b
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-01-21 23:31:27 +0900

    [Bronze I] Title: 단어 공부, Time: 164 ms, Memory: 115772 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,22 +1,39 @@
-dict = {}
+# dict = {}
+#
+# str = list((input().upper()))
+#
+# for s in str:
+#     dict[s] = 0;
+# for s in str:
+#     dict[s] = dict[s]+1;
+#
+# max = max(dict.values())
+# cnt = 0;
+# ans = ""
+#
+# for _ in dict.keys():
+#     if(dict[_]==max):
+#         cnt += 1
+#         ans = _
+#     if(cnt>=2):
+#         ans = "?"
+#         break;
+#
+# print(ans)
+from collections import Counter
 
-str = list((input().upper()))
+string = input().upper()
 
-for s in str:
-    dict[s] = 0;
-for s in str:
-    dict[s] = dict[s]+1;
+counter = Counter(string)
 
-max = max(dict.values())
-cnt = 0;
-ans = ""
+max_count = max(counter.values())
+most_common = []
 
-for _ in dict.keys():
-    if(dict[_]==max):
-        cnt += 1
-        ans = _
-    if(cnt>=2):
-        ans = "?"
-        break;
+for char, count in counter.items():
+    if count == max_count:
+        most_common.append(char)
 
-print(ans)
+if(len(most_common)==1):
+    print(most_common[0])
+else:
+    print("?")
