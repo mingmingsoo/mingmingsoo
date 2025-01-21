b5f62bac307a43c24e6b51b23101c5fb99c7b853
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-01-22 01:19:28 +0900

    [Silver V] Title: 다리 놓기, Time: 120 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -3,8 +3,8 @@ for t in range(T):
     n, m = map(int, input().split())
     # mCn
     mul = 1
-    for num in range(m-n+1, m+1):
+    for num in range(m, m-n,-1):
         mul *= num
     for num in range(1, n+1):
         mul //= num
-    print(int(mul))
+    print(mul)
