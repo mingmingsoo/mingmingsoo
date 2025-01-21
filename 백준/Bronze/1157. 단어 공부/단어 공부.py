9a9f247af2e0b78ff00f44ff2b5e85de49e6f22c
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-01-21 23:26:03 +0900

    [Bronze I] Title: 단어 공부, Time: 224 ms, Memory: 159028 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,22 +1,22 @@
-from collections import Counter
+dict = {}
 
-def main():
-    # 문자열 입력 받기
-    str_input = input().strip().upper()
-    
-    # 각 알파벳의 등장 횟수 세기
-    counter = Counter(str_input)
-    
-    # 등장 횟수가 가장 많은 알파벳 찾기
-    max_count = max(counter.values())
-    
-    # 가장 큰 값이 여러 개인지 확인
-    max_chars = [char for char, count in counter.items() if count == max_count]
-    
-    if len(max_chars) > 1:
-        print("?")
-    else:
-        print(max_chars[0])
+str = list((input().upper()))
 
-if __name__ == "__main__":
-    main()
+for s in str:
+    dict[s] = 0;
+for s in str:
+    dict[s] = dict[s]+1;
+
+max = max(dict.values())
+cnt = 0;
+ans = ""
+
+for _ in dict.keys():
+    if(dict[_]==max):
+        cnt += 1
+        ans = _
+    if(cnt>=2):
+        ans = "?"
+        break;
+
+print(ans)
