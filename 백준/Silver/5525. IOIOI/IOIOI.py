a3ef4129b795e21d0fa329f0dcdbe198997e3b79
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-01-26 16:06:46 +0900

    [Silver I] Title: IOIOI, Time: 128 ms, Memory: 113408 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -2,22 +2,20 @@ n = int(input()) # 2*n+1 개의 IOI가 나와야함
 m = int(input())
 line = input()
 cnt = 0
-for i in range(m-(2*n+1)+1):
-    if(line[i]=="I"):
-        # print(i)
-        isIO = True
-        for j in range(i,i+(2*n+1)):
-            # print(i,i+(2*n+1))
-            if((j-i)%2==0):
-                if(line[j]!='I'):
-                    isIO = False
-                    break
-            else:
-                if(line[j]!='O'):
-                    isIO = False
-                    break
-        if(isIO):
-            # print(i)
-            cnt+=1
-            continue
+i = 0
+while(i<m):
+    start = i
+    end = i
+    if(line[i]=='I'):
+        for j in range(i+1, m):
+            if((i-j)%2==0 and line[j]=='O'):
+                break
+            if((i-j)%2==1 and line[j]=='I'):
+                break
+            end = j
+            if(line[end]=='O'):
+                end = j-1
+    if(end-start+1 >2*n):
+        cnt += (end-start+1)//2-(n-1)
+    i = end+1
 print(cnt)
