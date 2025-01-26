7dabd7bd8db2f389974c9259158288950d3076be
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-01-27 08:59:46 +0900

    [Gold IV] Title: DSLR, Time: 12664 ms, Memory: 215120 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.py
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -1,39 +1,36 @@
-from collections import deque
-size = 10000
-
-def bfs(start, end):
-    q = deque([(start,[])])
-    visited = [0]*size
-    visited[start] = 0
-
-    while q:
-        now, order = q.popleft()
-        now = now%10000
-        if(now == end):
-            return order
-
-        stringNow = str(f"{now:04}")
-        LNow = int(stringNow[1:]+stringNow[0])
-        RNow =int(stringNow[-1]+stringNow[0:len(stringNow)-1])
-
-        if(visited[(2*now)%10000]==0):
-            q.append(((2*now)%10000,order+ ["D"]))
-            visited[(2*now)%10000]=1
-        if(visited[(now-1)%10000]==0):
-            q.append(((now-1)%10000,order+["S"]))
-            visited[(now-1)%10000] = 1
-        if(LNow<10000 and visited[LNow]==0):
-            q.append(((LNow,order +["L"])))
-            visited[LNow] = 1
-        if(RNow<10000 and visited[RNow]==0):
-            q.append(((RNow,order+ ["R"])))
-            visited[RNow] = 1
-
 T = int(input())
 for tc in range(T):
-    start, end = map(int, input().split())
-    ans = bfs(start ,end)
-    for s in ans:
-        print(s, end = "")
-    print()
+    from collections import deque
 
+    start, end = map(int, input().split())
+
+    visited = [False] * 10000
+    ans = ""
+
+    q = deque([(start, ans)])
+    visited[start] = True
+
+    while q:
+        cur, curAns = q.popleft()
+        if (cur == end):
+            print(curAns)
+            break
+        mul2 = (cur * 2) % 10000
+        minus1 = (cur - 1) % 10000
+
+        strNum = str(f"{cur:04}")
+        strL = strNum[1:] + strNum[0]
+        strR = strNum[3] + strNum[:3]
+
+        if (not visited[mul2]):
+            q.append((mul2, curAns + "D"))
+            visited[mul2] = True
+        if (not visited[minus1]):
+            q.append((minus1, curAns + "S"))
+            visited[minus1] = True
+        if (not visited[int(strL)]):
+            q.append((int(strL), curAns + "L"))
+            visited[int(strL)] = True
+        if (not visited[int(strR)]):
+            q.append((int(strR), curAns + "R"))
+            visited[int(strR)] = True
