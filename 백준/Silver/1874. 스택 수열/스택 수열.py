c755b60b91a61666db9a7ebec95fed988b2f1253
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-01-31 15:02:06 +0900

    [Silver II] Title: 스택 수열, Time: 188 ms, Memory: 119200 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,27 +1,24 @@
 n = int(input())
 stack = []
 idx = 1
-ans = ""
-first = int(input())
-while (idx <= first):
-    stack.append(idx)
-    idx += 1
-    ans += "+\n"
-stack.pop()
-ans += "-\n"
-for i in range(n-1):
+ans = []
+for i in range(n):
     num = int(input())
     while (idx <= num):
         stack.append(idx)
         idx += 1
-        ans+="+\n"
+        ans.append("+")
     while(stack and stack[-1]!=num):
         stack.pop()
-        ans+="-\n"
+        ans.append("-")
     if(not stack):
         ans = "NO"
         break
     elif(stack[-1]==num):
         stack.pop()
-        ans+="-\n"
-print(ans)
+        ans.append("-")
+if(ans =="NO"):
+    print(ans)
+else:
+    for x in ans:
+        print(x)
