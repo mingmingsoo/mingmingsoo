2dfee801dcad8df919ce35e7ef3f99c735612cda
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-02-03 08:30:00 +0900

    [Gold IV] Title: 오큰수, Time: 448 ms, Memory: 248732 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,28 +1,13 @@
-# n = int(input())
-# arr = list(map(int, input().split()))
-# stack = [0]
-# ans = [-1] * n
-#
-# for i in range(1,n):
-#
-#     while stack and arr[stack[-1]]< arr[i]:
-#         ans[stack.pop()] = arr[i]
-#     stack.append(i)
-#
-# print(*ans)
-
 n = int(input())
 arr = list(map(int, input().split()))
-stack = [arr[-1]]
-ans = [-1]*n
+ans = [-1]* n
+stk = [arr[-1]]
 for i in range(n-2,-1,-1):
-    # stack에 있는 값이 나보다 작으면 다 빼낸다.
-    while( stack and arr[i]>=stack[-1]):
-        stack.pop()
-    # 나보다 큰놈이 있으면(== 스택이 남아있으면) 그게 오큰수다.
-    # print(stack)
-    if(stack):
-        ans[i] = stack[-1]
-    # 내 왼쪽 값들은 나보다 작을 수 있어서 stack에 넣어준다.
-    stack.append(arr[i])
-print(*ans)
+
+    while stk and arr[i]>=stk[-1]:
+        stk.pop()
+    if stk and arr[i]<stk[-1]:
+        ans[i] = stk[-1]
+    stk.append(arr[i])
+for char in ans:
+    print(char, end = " ") # 출력때문에 한번 틀렸음.
