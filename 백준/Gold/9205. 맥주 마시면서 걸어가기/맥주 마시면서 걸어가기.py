93ae9087c3d7da1c7c9a3fbfeea147778603a8be
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-02-05 15:31:00 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 132 ms, Memory: 111420 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,45 +1,42 @@
-'''
-한 박스에 20개
-50미터에 한 병씩(마시고 출발)
-
-편의점을 다 갈 수 있는게 아니라 한곳만 갈 수 있나봄.??
-'''
-
 T = int(input())
-
-
-def dfs(start, end):
-    global ans
-    visited[start] = True
-    r,c,idx = dist[start]
-
-    if(idx==end):
-        ans = "happy"
-        return
-
-    for i in range(store+2):
-        if(i==start):
-            continue
-        nr, nc,nidx = dist[i]
-        if(not visited[i] and abs(nr-r)+abs(nc-c)<=1000):
-            dfs(i,end)
-
-
 for tc in range(T):
+    '''
+    풀었던 문제
+    
+    단방향
+    노드들의 거리가 1000이하면 방문 가능함. -> 간선이 연결될 수 있음
+    노드들에 idx 부여해서 idx 도달하면 happy
+    
+    '''
+
     store = int(input())
-    visited = [False] * (store+2)
-    dist = []
+    adj = []  # 집,락, 그리고 편의점들
     hr, hc = map(int, input().split())
-    dist.append((hr,hc,0))
-    for i in range(1, store+1):
-        sr, sc = map(int, input().split())
-        dist.append((sr, sc,i))
-    fr, fc = map(int, input().split())
-    dist.append((fr, fc,store+1))
+    adj.append((hr, hc, 0))
+    for i in range(store):
+        cr, cc = map(int, input().split())
+        adj.append((cr, cc, i + 1))
+    rr, rc = map(int, input().split())
+    adj.append((rr, rc, store+1))
 
-    ans ="sad"
-    dfs(0,store+1)
+    ans = "sad"
+    visited = [False] * (store + 2)
+
+    def dfs(start, end):
+        global ans
+
+        visited[start] = True
+
+        if (start == end):
+            ans = "happy"
+            return
+
+        for i in range(store + 2):
+            if (visited[i]):
+                continue
+            if (abs(adj[start][0] - adj[i][0]) + abs(adj[start][1] - adj[i][1]) <= 1000):
+                dfs(i, end)
+
+
+    dfs(0, store + 1)
     print(ans)
-
-
-
