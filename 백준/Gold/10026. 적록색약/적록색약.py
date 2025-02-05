1046f345ddcd35396372c70db764f9f56d713b31
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-02-05 15:08:02 +0900

    [Gold V] Title: 적록색약, Time: 136 ms, Memory: 115132 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,52 +1,55 @@
+'''
+풀었던 문제
+
+정상인이 보는 군집의 갯수와
+색약이 보는 군집의 갯수 출력
+
+해결 방법
+색약이 보는 2차원 배열을 새로 생성해서 bfs로 돌리거나
+bfs에서 q에 넣어주는 조건을 r or g로 하면 된다.
+
+'''
 from collections import deque
+def count(grid):
+    colors = 0
+    visited = [[False] * n for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if(not visited[i][j]):
+                bfs(i,j,grid[i][j],grid,visited)
+                colors+=1
+    return colors
 
-n = int(input())
-grid = [list(input()) for i in range(n)]
-
-
-gridNoColor = [[0]*n for i in range(n)]
-for i in range(0,n):
-    for j in range(0,n):
-        if(grid[i][j]!="G"):
-            gridNoColor[i][j] = grid[i][j]
-        else:
-            gridNoColor[i][j] = 'R'
-
-
-row = [-1,1,0,0]
-col = [0,0,1,-1]
-def bfsColor(i, j, arr, color):
-    global visited
-    q = deque()
-    q.append((i,j))
-    visited.add((i,j))
+def bfs(i, j, color, grid,visited):
+    q = deque([(i, j)])
+    visited[i][j] = True
 
     while q:
-        r,c = q.popleft()
+        r, c = q.popleft()
         for k in range(4):
             nr = r + row[k]
             nc = c + col[k]
-            if (0 <= nr < n and 0 <= nc < n and (nr, nc) not in visited and arr[nr][nc] == color):
-                q.append((nr, nc))
-                visited.add((nr, nc))
+            if (not (0 <= nr < n and 0 <= nc < n)):
+                continue
+            if (not visited[nr][nc] and grid[nr][nc]==color):
+                q.append((nr,nc))
+                visited[nr][nc] = True
+                
+n = int(input())
+rgb = [list(input()) for i in range(n)]
+
+rb = [[0]*n for i in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if(rgb[i][j]=="B"): # 파란색만 제대로 볼 수 있음
+            rb[i][j] = "B"
+        else:
+            rb[i][j] = "G"
+
+row = [-1,1,0,0]
+col = [0,0,1,-1]
 
 
-def ColorCount(arr):
-    total = 0
-    for i in range(n):
-        for j in range(n):
-            if ((i, j) not in visited):
-                bfsColor(i, j, arr, arr[i][j])
-                total += 1
-    return total
 
-
-
-visited = set()
-yes = ColorCount(grid)
-
-visited = set()
-no = ColorCount(gridNoColor)
-
-
-print(yes, no)
+print(count(rgb),count(rb))
