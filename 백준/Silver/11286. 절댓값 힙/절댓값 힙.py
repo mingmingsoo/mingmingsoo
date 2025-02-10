240c7df76668e1f3198f9791e4192a9717e09109
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-02-10 13:36:22 +0900

    [Silver I] Title: 절댓값 힙, Time: 184 ms, Memory: 116412 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,22 +1,16 @@
 import heapq
 import sys
-class num:
-    def __init__(self, n):
-        self.n = n
-    def __lt__(self, other):
-        if(abs(self.n)==abs(other.n)):
-            return self.n<other.n
-        else:
-            return abs(self.n)<abs(other.n)
+
 input = sys.stdin.readline
 n = int(input())
 pq = []
 for i in range(n):
     order = int(input())
     if(order!=0):
-        heapq.heappush(pq, num(order))
+        heapq.heappush(pq, (abs(order),order))
     else:
         if(not pq):
             print(0)
             continue
-        print(heapq.heappop(pq).n)
+        absNum, origin = heapq.heappop(pq)
+        print(origin)
