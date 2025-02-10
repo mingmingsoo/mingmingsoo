a7066759d4dc36d8c8177ce5ed4e27218d329c6e
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-02-10 19:28:48 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 3028 ms, Memory: 122848 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,19 +1,14 @@
-n, M = map(int, input().split())
+n,m = map(int, input().split())
 arr = list(map(int, input().split()))
-# print(arr)
 
-sumArray = [0]*n
-sumArray[0] = arr[0]
+sum_arr = [0]*n
+sum_arr[0] = arr[0]
 for i in range(1,n):
-    sumArray[i] = sumArray[i-1]+arr[i]
-# print(sumArray)
-for m in range(M):
-    start, end = map(int, input().split())
-    start -= 1
-    end -=1
-    if(start ==0):
-        print(sumArray[end])
-    else:
-        print(sumArray[end]-sumArray[start-1])
+    sum_arr[i]= arr[i]+sum_arr[i-1]
+sum_arr.insert(0,0)
+# print(sum_arr)
 
-
+for i in range(m):
+    s, e = map(lambda x:int(x)-1, input().split())
+    ans = sum_arr[e+1] - sum_arr[s]
+    print(ans)
