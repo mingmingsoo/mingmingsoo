70ef3aae672b56c246289aad7eb7d7ba6bd2e932
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-02-11 21:35:40 +0900

    [Silver III] Title: 거북이, Time: 552 ms, Memory: 111488 KB -BaekjoonHub

--- 백준/Silver/8911. 거북이/거북이.py
+++ 백준/Silver/8911. 거북이/거북이.py
@@ -1,19 +1,23 @@
+'''
+문제설명
+    명령이 주어졌을 때 거북이가 이동한 영역을 구해라.
+
+구상
+    이동할 때마다 최소 최대 x,y를 갱신해서 넓이를 구한다.
+    2차원 배열처럼 생겼지만 굳이 그렇게 안해도 된다.
+
+제츌횟수 4회
+틀린이유  d = 1 을 d ==1 로 썼음.....
+
+'''
 T = int(input())
 for tc in range(T):
-    '''
-    문제설명
-        명령이 주어졌을 때 거북이가 이동한 영역을 구해라.
-
-    구상
-        이동할 때마다 최소 최대 x,y를 갱신해서 넓이를 구한다.
-        2차원 배열처럼 생겼지만 굳이 그렇게 안해도 된다.
-    '''
 
     order_list = list(input())
     minR, maxR, minC, maxC = 0, 0, 0, 0
-    # 북 왼 우 남
-    row = [1, 0, 0, -1]
-    col = [0, -1, 1, 0]
+    # 북 동 남 서
+    row = [1, 0, -1, 0]
+    col = [0, 1, 0, -1]
     r, c, d = 0, 0, 0  # 시작점과 방향
     for order in order_list:
         if (order == "F"):
@@ -23,23 +27,9 @@ for tc in range(T):
             r = r - row[d]
             c = c - col[d]
         elif (order == "L"):
-            if(d==0):
-                d = 1
-            elif(d==1):
-                d= 3
-            elif(d==2):
-                d=0
-            elif(d==3):
-                d = 2
+            d = (d-1+4)%4
         elif (order == "R"):
-            if(d==0):
-                d = 2
-            elif(d==1):
-                d= 0
-            elif(d==2):
-                d=3
-            elif(d==3):
-                d = 1
+            d = (d+1) % 4
         minR = min(minR, r)
         minC = min(minC, c)
         maxR = max(maxR, r)
