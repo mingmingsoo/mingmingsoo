6c4ae880bf7082d459362252795e5d0432f0ecb7
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-02-12 09:39:59 +0900

    [Silver IV] Title: 동전 0, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,19 +1,14 @@
 n, k = map(int, input().split())
-
 arr = []
 for i in range(n):
     arr.append(int(input()))
-arr.sort(reverse=True)
+
+arr.sort(reverse= True) # 큰 숫자들부터 차례차례 확인한다
 ans = 0
-for num in arr:
-    cnt = 0
-    if(num<=k):
-        cnt += (k//num)
-        k -= (cnt * num)
-
-    ans += cnt
-
-    if(k <=0):
+for coin in arr:
+    if(coin<=k): # 만약 돈이 목표 금액보다 작으면 계산 가능
+       ans += (k//coin) # 몫 만큼 더하고
+       k = k -  (k //coin)*coin # 내가 가진 돈은 그만큼 빼준다
+    if(k==0):
         break
-
 print(ans)
