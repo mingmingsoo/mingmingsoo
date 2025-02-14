2ee64ad31bfa29b311bae72bd909a6a7cb4dda32
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-02-14 09:43:55 +0900

    [Silver II] Title: 나무 자르기, Time: 428 ms, Memory: 258500 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,35 +1,41 @@
 '''
-m 부터 배열의 최대값까지
-이분탐색면서 나머지의 합이 m이 되는...
+적어도 M미터의 나무를 가져가기 위해 절단기에 설정할 수 있는 높이의 최댓값.
+최대한 높이..
+일단 조건을 만족하는 높이를 구하고. 높이를 조금씩 올려가기
+
+반례
+4 80
+20 20 20 20
+
+나무를 다 잘라야 하는 수가 있음
+그래서 ans 를 0을 초깃값으로 설정해줘야함
 
 '''
-
-n, m = map(int, input().split())
+n,m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-
-start = 0
+start = 0 # 여기서  틀림!
 end = max(arr)
-mid = (start+end)//2
-ans = 0
 
-def calSum(arr, mid):
-    num = 0
-    for i in arr:
-        if(i>mid):
-            num+=(i-mid)
-    return num
+ans = 0 # 최댓값 출력이므로,
 
-# 2 1
-# 2 2 답 1
-while(start<=mid):
-    sum = calSum(arr, mid)
-    # print(start, mid, end, sum)
-    if(sum>=m):
-        ans = mid
-        start = mid+1
-        mid = (start+end)//2
-    elif(sum <m):
-        end = mid-1
-        mid = (start+end)//2
-print(int(ans))
+def isOk(middle):
+    add = 0
+    for tree in arr:
+        if(tree>middle):
+            add += tree-middle
+    if add >= m:
+        return True
+    else:
+        return False
+
+while start<=end:
+    middle = (start+end)//2
+
+    if(isOk(middle)): # 집에갈 수 있어 -> start를 좀만 높여보자.
+        ans = middle
+        start = middle+1
+    else: # 좀만 낮혀보자
+        end  = middle-1
+
+print(ans)
