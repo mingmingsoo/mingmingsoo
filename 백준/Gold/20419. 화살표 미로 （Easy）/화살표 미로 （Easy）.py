9fbe3464931108438253d0a452c88149d5b49c78
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-02-16 23:34:10 +0900

    [Gold III] Title: 화살표 미로 (Easy), Time: 132 ms, Memory: 114920 KB -BaekjoonHub

--- 백준/Gold/20419. 화살표 미로 （Easy）/화살표 미로 （Easy）.py
+++ 백준/Gold/20419. 화살표 미로 （Easy）/화살표 미로 （Easy）.py
@@ -15,52 +15,34 @@ for i in range(n):
 ans = "No"
 row = [-1, 0, 1, 0]
 col = [0, 1, 0, -1]
-
-
 def bfs(sr, sc, er, ec):
     global ans
     q = deque()
-    q.append((sr, sc, grid[sr][sc], card, card))
-    visited = [[[[[False] * (card + 1) for c in range(card + 1)] for d in range(4)] for mm in range(m)] for nn in
+    q.append((sr, sc ,card, card))
+    visited = [[[[False] * (card + 1) for c in range(card + 1)] for mm in range(m)] for nn in
                range(n)]
-    visited[sr][sc][grid[sr][sc]][card][card] = True
-    for i in range(1, card + 1):
-        if (0 <= sr + row[(grid[sr][sc] + i) % 4] < n and 0 <= sc + col[(grid[sr][sc] + i) % 4] < m):
-            visited[sr][sc][(grid[sr][sc] + i) % 4][card][card - i] = True
-            q.append((sr, sc, (grid[sr][sc] + i) % 4, card, card - i))
-        if (0 <= sr + row[(grid[sr][sc] - i + 4) % 4] < n and 0 <= sc + col[(grid[sr][sc] - i + 4) % 4] < m):
-            visited[sr][sc][(grid[sr][sc] - i + 4) % 4][card - i][card] = True
-            q.append((sr, sc, (grid[sr][sc] - i + 4) % 4, card - i, card))
+    visited[sr][sc][card][card] = True
     while q:
-        r, c, d, left, right = q.popleft()
-        # print( r, c,"URDL"[d], left, right)
+        r, c, left, right = q.popleft()
         if r == er and c == ec:
             ans = "Yes"
             return
-        nr = r + row[d]
-        nc = c + col[d]
-        if nr == er and nc == ec:
-            ans = "Yes"
-            return
-        if not (0 <= nr < n and 0 <= nc < m):
-            continue
-        next_d = grid[nr][nc]
 
-        if not visited[nr][nc][grid[nr][nc]][left][right]:
-            if (0 <= nr + row[next_d] < n and 0 <= nc + col[next_d] < m):
-                visited[nr][nc][grid[nr][nc]][left][right] = True
-                q.append((nr, nc, next_d, left, right))
-        for i in range(1, left + 1):
-            if not visited[nr][nc][(next_d - i + 4) % 4][left - i][right]:
-                if (0 <= nr + row[(next_d - i + 4) % 4] < n and 0 <= nc + col[(next_d - i + 4) % 4] < m):
-                    visited[nr][nc][(next_d - i + 4) % 4][left - i][right] = True
-                    q.append((nr, nc, (next_d - i + 4) % 4, left - i, right))
-        for i in range(1, right + 1):
-            if not visited[nr][nc][(next_d + i) % 4][left][right - i]:
-                if (0 <= nr + row[(next_d + i) % 4] < n and 0 <= nc + col[(next_d + i) % 4] < m):
-                    visited[nr][nc][(next_d + i) % 4][left][right - i] = True
-                    q.append((nr, nc, (next_d + i) % 4, left, right - i))
+        for k in range(4):
+            nr = r+row[k]
+            nc = c+col[k]
+            if not (0 <= nr < n and 0 <= nc < m):
+                continue
+            right_count = (4 + grid[r][c] - k) % 4
+            if right - right_count >= 0 and not visited[nr][nc][left][right - right_count]:
+                q.append((nr,nc,left,right-right_count))
+                visited[nr][nc][left][right - right_count] = True
 
+            left_count = (4 + k - grid[r][c]) % 4
+            if left - left_count >= 0 and not visited[nr][nc][left - left_count][right]:
+                q.append((nr, nc, left - left_count, right))
+                visited[nr][nc][left - left_count][right] = True
 
-bfs(0, 0, n - 1, m - 1)
+bfs(0,0,n-1,m-1)
+
 print(ans)
