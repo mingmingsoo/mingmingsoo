a82b1cd82a1a2a5d53c22acf6e7f93765ebf7c43
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-02-18 13:41:49 +0900

    [Silver I] Title: 회전초밥, Time: 396 ms, Memory: 168528 KB -BaekjoonHub

--- 백준/Silver/28107. 회전초밥/회전초밥.py
+++ 백준/Silver/28107. 회전초밥/회전초밥.py
@@ -1,20 +1,26 @@
+'''
+초밥을 줄 때 무조건 1번손님부터 줘서(작은번호의 손님부터)
+힙 큐를 사용해서 초밥을 관리한다
+'''
+
 import heapq
 import sys
 input = sys.stdin.readline
+
 N, M = map(int, input().split())
-size = 200_001
-# size = 10
+size = 200_001 # 최대 초밥 갯수
 chobab = {}
 for i in range(size):
-    chobab[i]  = []
+    chobab[i]  = [] # 초밥을 힙큐로 관리할 것임
+    
 for i in range(N):
     tmp = list(map(int,input().split()))
     for j in range(1,len(tmp)):
-        heapq.heappush(chobab[tmp[j]], i)
+        heapq.heappush(chobab[tmp[j]], i) # 초밥 번호마다 손님 번호를 기록해줌
 
 order= list(map(int, input().split()))
 count = [0]*N
 for num in order:
-    if(chobab[num]):
-        count[heapq.heappop(chobab[num])]+=1
+    if(chobab[num]): # 초밥시키신 빠른 손님
+        count[heapq.heappop(chobab[num])]+=1 # 나오세요
 print(*count)
