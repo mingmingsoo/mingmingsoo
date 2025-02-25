98db7c7b8299176e376fe6cf9b731d6984d017e6
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-02-25 22:18:46 +0900

    [Gold IV] Title: 미로만들기, Time: 136 ms, Memory: 111988 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,8 +1,12 @@
 '''
+# 제출 횟수 : 1회
+# 메모리 : 112044KB
+# 실행 시간 : 140ms
+
 b2665
-흠냐 백트래킹인가.
+백트래킹인가.
 근데 맵이 큰데..
-다익스트라 응용
+다익스트라 응용으로 풀겠음
 
 구상
     1인애들은 그냥지나가고 0을 d에 기록
@@ -12,8 +16,8 @@ import heapq
 n = int(input())
 grid = [list(map(int, input())) for i in range(n)]
 
-d = [[2501] * n for i in range(n)]
-d[0][0] = 0
+d = [[2501] * n for i in range(n)] # 큰 값으로 채우기
+d[0][0] = 0 # 시작점
 
 def dijk(sr,sc,er,ec):
     row = [-1,1,0,0]
@@ -26,7 +30,7 @@ def dijk(sr,sc,er,ec):
         if(r==er and c == ec):
             break
 
-        if(dist > d[r][c]):
+        if(dist > d[r][c]): # 내가 온 길이 돌아온 길이면 continue
             continue
         for k in range(4):
             nr = r+row[k]
@@ -34,13 +38,9 @@ def dijk(sr,sc,er,ec):
             if not(0<=nr<n and 0<=nc<n):
                 continue
 
-            if grid[nr][nc] ==1 and d[nr][nc]==2501:
-                d[nr][nc] = d[r][c]
-                heapq.heappush(q,(dist,nr,nc))
-            elif grid[nr][nc]==0:
-                if d[nr][nc] > dist+1:
-                    d[nr][nc] = dist+1
-                    heapq.heappush(q,(dist+1,nr,nc))
+            if d[nr][nc] > dist+(1-grid[nr][nc]):
+                d[nr][nc] = dist+(1-grid[nr][nc])
+                heapq.heappush(q,(d[nr][nc],nr,nc))
 
 dijk(0,0,n-1,n-1)
 
