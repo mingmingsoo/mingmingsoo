6597ae1aec9f47582fe91529ee86cb5bf1ad225b
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-02 14:54:22 +0900

    [Gold I] Title: 구슬 탈출, Time: 368 ms, Memory: 182488 KB -BaekjoonHub

---
+++ 백준/Gold/13459. 구슬 탈출/구슬 탈출.py
@@ -0,0 +1,119 @@
+'''
+13:48~
+문제설명
+    10번 이하로 빨간 구슬을 구멍을 통해 빼낼 수 있는지?
+    왼/오/위/아 기울이기 가능
+    1. 파란구슬 빠지면 실패
+    2. 빨, 파 동시에 빠져도 실패
+    3. 구슬이 움직이지 않으면 그만.
+구상
+    bfs R과 B 방향을 같이 관리해줘야함.
+    O에 도달하면.....
+    종료조건을 봐줘야할 듯
+오해 10 번이 좌라라라라락 10번이였음.
+'''
+from collections import deque
+
+n, m = map(int, input().split())
+grid = [list(input()) for i in range(n)]
+
+ar,ac,br,bc = -1,-1,-1,-1
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == "R":
+            ar,ac = i,j
+            grid[i][j] = "."
+        elif grid[i][j] == "B":
+            br, bc = i, j
+            grid[i][j] = "."
+isOk = False
+def bfs(ar,ac,br,bc):
+    global isOk
+    q = deque([(ar,ac,br,bc,0)]) # 빨, 파 같이 관리해줘야함
+    # visited 는 쓰지 않는다
+    row = [0,1,0,-1]
+    col = [-1,0,1,0]
+    while q:
+        ar,ac,br,bc,time = q.popleft()
+        # print(ar,ac,br,bc,time)
+        if grid[ar][ac] =="O" and grid[br][bc] != "O":
+            isOk = True
+            return
+        if grid[br][bc] =="O":
+            continue
+        origin_ar, origin_ac,origin_br,origin_bc = ar,ac,br,bc
+        if time >10:
+            return # 끝
+    # 왼/오/위/아 기울이기 가능
+        for k in range(4):
+            ar, ac, br, bc = origin_ar, origin_ac,origin_br,origin_bc
+            while True:
+                # 갈 수 있으면
+                anr = ar+row[k]
+                anc = ac+col[k]
+                if 0 <= anr < n and 0 <= anc < m and grid[anr][anc] == ".":
+                    ar = anr
+                    ac = anc
+                elif 0 <= anr < n and 0 <= anc < m and grid[anr][anc] == "O":
+                    ar = anr
+                    ac = anc
+                    break
+                else:
+                    break
+            while True:
+                bnr = br+row[k]
+                bnc = bc+col[k]
+                # 갈 수 있으면
+                if 0 <= bnr < n and 0 <= bnc < m and grid[bnr][bnc] == ".":
+                    br = bnr
+                    bc = bnc
+                elif 0 <= bnr < n and 0 <= bnc < m and grid[bnr][bnc] == "O":
+                    br = bnr
+                    bc = bnc
+                    break
+                else:
+                    break
+            if origin_ar == ar and origin_ac == ac and origin_br == br and origin_bc == bc:
+                continue # 이동이 없었으면 넘어가
+            if ar == br and ac == bc and grid[ar][ac] == ".":
+                # 만약 같았다........면 누가 먼저 왔는지 확인해줘야하는데.
+                if k == 0:
+                    # 서쪽으로 왔을 땐
+                    # 오리지널이 c가 큰애가 하나 오른쪽으로
+                    if(origin_ac < origin_bc):
+                        bc += 1
+                    else:
+                        ac +=1
+                elif k == 1:
+                    # 남쪽이면 orign r이 작은 애가 -1 해줌
+                    if(origin_ar > origin_br):
+                        br -= 1
+                    else:
+                        ar -= 1
+                elif k == 2:
+                    # 동쪽이면 orign c이 작은 애가 -1 해줌
+                    if(origin_ac > origin_bc):
+                        bc -= 1
+                    else:
+                        ac -= 1
+                elif k == 3:
+                    # 북쪽이면 orign r이 큰애 애가 +1 해줌
+                    if(origin_ar < origin_br):
+                        br += 1
+                    else:
+                        ar += 1
+            q.append((ar,ac,br,bc,time+1))
+
+    # 1. 파란구슬 빠지면 실패
+    # 2. 빨, 파 동시에 빠져도 실패
+    # 3. 구슬이 움직이지 않으면 그만. o
+    # 같은 위치는 불가능 o
+
+
+
+
+bfs(ar,ac,br,bc)
+if isOk:
+    print(1)
+else:
+    print(0)
