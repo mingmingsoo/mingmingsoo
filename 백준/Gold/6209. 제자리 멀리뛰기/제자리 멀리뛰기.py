a91397e078b4eed9f3d9d1c5be17bc76ed76cd35
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-04 08:45:00 +0900

    [Gold II] Title: 제자리 멀리뛰기, Time: 148 ms, Memory: 126540 KB -BaekjoonHub

--- 백준/Gold/6209. 제자리 멀리뛰기/제자리 멀리뛰기.py
+++ 백준/Gold/6209. 제자리 멀리뛰기/제자리 멀리뛰기.py
@@ -1,46 +1,50 @@
 '''
+# 제출횟수: 6회
+# 실행시간: 148ms
+# 메모리: 127052 KB
+
 답이 되는 거리를 이분탐색
+만약 간격이 middle보다 작으면 그 돌을 없애보고,
+m개만큼 제거가 되면 start 늘려보기
+아니면 값 조정 필요 end 줄이기
 '''
 
-end_origin, n, m = map(int, input().split())
+
+def isOk(middle):
+    remove = 0
+    arr = arr_origin[:]
+
+    for i in range(len(arr)):
+        if arr[i] < middle:
+            if remove < m:
+                remove += 1
+                arr[i + 1] += arr[i]
+            else:
+                return False
+    return True
+
+
+end, n, m = map(int, input().split())
 tmp = [int(input()) for i in range(n)]
 tmp.sort()
 tmp.insert(0, 0)
-tmp.append(end_origin)
+tmp.append(end)
 arr = []
 for i in range(len(tmp) - 1):
     arr.append(tmp[i + 1] - tmp[i])
 # print(arr)
 
 start = 0
-end = end_origin
 ans = 0
 arr_origin = arr[:]
 
-
-def isOk(middle):
-    remove = 0
-    idx = 0
-    arr = arr_origin[:]
-
-    while idx <= len(arr) - 1:
-        if arr[idx] < middle:
-            if remove < m:
-                remove += 1
-                arr[idx + 1] += arr[idx]
-                idx += 1
-            else:
-                return False
-        else:
-            idx += 1
-    return True
-
-
 while start <= end:
+
     middle = (start + end) // 2
-    if isOk(middle):
+    if isOk(middle): # 가능하다면 늘려보기
         ans = middle
         start = middle + 1
-    else:
+    else: # 안되면 줄이기
         end = middle - 1
+
 print(ans)
