6b68ff3fbf0a460941d6f5b85180e9896f858736
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-04 20:25:26 +0900

    [Gold V] Title: 선물이 넘쳐흘러, Time: 108 ms, Memory: 111036 KB -BaekjoonHub

--- 백준/Gold/17259. 선물이 넘쳐흘러/선물이 넘쳐흘러.py
+++ 백준/Gold/17259. 선물이 넘쳐흘러/선물이 넘쳐흘러.py
@@ -1,105 +1,72 @@
 '''
-문제설명
-    디귿 반대모양의 컨베이어 벨트
-    M개의 선물을 포장하기 위해 N명의 직원을 고용
-    선물 포장하는 시간은 다 다름
-    직원들은 상하좌우 벨트 포장 가능 ->. 더 오래된거 포장
+선물이 넘쳐흘러 다시 풀기
 
+5 1 2
+1 0 1
+
+정답2
 '''
 
-n, m, present = map(int, input().split()) # 맵 크기, 직원, 선물갯수
-grid = [[0]*(n+1) for i in range(n)]
-people = [[False]*(n+1) for i in range(n)]
-people_origin = [[0]*(n+1) for i in range(n)]
+
+class Person:
+    def __init__(self, r, c, time):
+        self.r = r
+        self.c = c
+        self.time = time
+        self.ing = 0
 
 
-for mm in range(m):
-    r,c,t =map(int, input().split())
-    grid[r][c+1] = t
-    people[r][c+1] = True
-    people_origin[r][c+1] = t
-
-# for _ in grid:
-#     print(_)
-# for _ in people:
-#     print(_)
-# for _ in people_origin:
-#     print(_)
-
-grid[0][0] = present
-row1 = [0,-1,0,1] # 오른쪽 위 모서리
-col1 = [1,0,-1,0]
-
-row2 = [1,0,-1,0] # 오른쪽 아래 모서리
-col2 = [0,1,0,-1]
-
+n, people_num, present_num = map(int, input().split())
+people_list = []
+for i in range(people_num):
+    r, c, t = map(int, input().split())
+    people_list.append(Person(r, c + 1, t))
+row = [1, 0, -1]
+col = [0, 1, 0]
+grid = [[0] * (n + 1) for i in range(n)]
+grid[0][0] = present_num  # 선물 놓는다.
 out = 0
-# for _ in grid:
-#     print(*_)
-time = 0
-while time<(n*2+n-2+present):
-    time+=1
-    #  올려!
-    if(grid[0][0]>0):
+for p in range(n * 2 + (n - 2) + present_num):
+    # 1. 선물이 있으면 올리기
+    if grid[0][0] > 0:
         grid[0][1] = 1
-        grid[0][0]-=1
+        grid[0][0] -= 1
 
-    # 포장중인 애들은 포장만해.
-    for i in range(1,n-1):
-        for j in range(1,n):
-            if people[i][j] and grid[i][j]!= people_origin[i][j]:
-                grid[i][j] -=1
+    # 3. 근방에 직원 있으면 포장
+    for people in people_list:
+        # 포장중이 아닌 애들은 포장!
+        if people.ing == 0:
+            for k in range(3):
+                nr = people.r + row[k]
+                nc = people.c + col[k]
+                if not (0 <= nr < n and 0 <= nc < n + 1):
+                    continue
+                if grid[nr][nc] > 0:
+                    grid[nr][nc] = 0
+                    people.ing += 1
+                    break
+        # 나머지들은 마저 포장!
+        else:
+            people.ing += 1
 
+    for people in people_list:
+        # 포장중이 아닌 애들은 포장!
+        if people.ing >= people.time:
+            people.ing = 0
 
-    # 숫자 이면서 visited가 True 인 데에서 상하 좌우에 숫자가 있으면 포장해
-    for i in range(1,n-1):
-        for j in range(1,n):
-            if people[i][j] and grid[i][j]== people_origin[i][j]: # 상자포장가능
-                if(i == n-2 and j == n-1):
-                    for k in range(4):
-                        nr = i + row2[k]
-                        nc = j + col2[k]
-                        if (grid[nr][nc] > 0 and not people[nr][nc]):
-                            grid[nr][nc] = 0
-                            grid[i][j] -= 1  # 포장중
-                            break
-                else:
-                    for k in range(4):
-                        nr = i+row1[k]
-                        nc = j+col1[k]
-                        if(grid[nr][nc]>0 and not people[nr][nc]):
-                            grid[nr][nc] = 0
-                            grid[i][j] -= 1 # 포장중
-                            break
-
-    # 그리고 0이면 시간 채워줘
-    for i in range(1,n-1):
-        for j in range(1,n):
-            if people[i][j] and grid[i][j]==0:
-                grid[i][j] = people_origin[i][j]
-
-    # 회전
-    # 뒤에서부터 댕기기
+    # 2. 이동
+    # 뒤에서 부터 당기기
     for j in range(n):
-        grid[n-1][j] = grid[n-1][j+1]
-
-    for i in range(n-1,-1,-1):
-        grid[i][n] = grid[i-1][n]
-
-    for j in range(n,1,-1):
-        grid[0][j] = grid[0][j-1]
+        grid[n - 1][j] = grid[n - 1][j + 1]
+    for i in range(n - 1, 0, -1):
+        grid[i][n] = grid[i - 1][n]
+    for j in range(n, 1, -1):
+        grid[0][j] = grid[0][j - 1]
     grid[0][1] = 0
 
-    # 떨어지는 애 검사
-    if(grid[n-1][0]>0):
-        out+=1
 
-
-
-
-
-    # print("-------------")
-    # for _ in grid:
-    #     print(*_)
-
-print(present-out)
+    # 4. 떨어지는 갯수 세기
+    if grid[n - 1][0] > 0:
+        out += 1
+        grid[n - 1][0] = 0
+print(present_num - out)
