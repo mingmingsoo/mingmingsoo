31cf914656a76f9a89aed47deca99361a54fb8af
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-05 15:44:15 +0900

    [Gold IV] Title: 이차원 배열과 연산, Time: 132 ms, Memory: 111476 KB -BaekjoonHub

--- 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.py
+++ 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.py
@@ -1,99 +1,64 @@
 '''
-문제 설명
-    갯수에 따른 연산.
-    R연산: 행에 대해 정렬 단 행 갯수>=열갯수
-    C연산: 열에 대해 정렬 단 열갯수>행갯수
 
-    정렬 기준
-    수의 등장 횟수가 커지는 순으로, 같다면 수가 커지는 순으로
-
-    3 1 1 2
-    2 1 3 1 1 2
-
-    만약 가장 큰 행/열 을 기준으로 크기가 변함. 모자란 곳은 0을 넣어줌
-    100을 넘어가면 100까지만.
-
-출력
-    A[r][c] 가 k가 되기위한 최소 시간.
-    100초가 지나도 달성하지 못하면 -1 출력
-
-구상
-    성실히 구현
+# 백준 17140 이차원 배열과 연산 (코드트리 격자 숫자 놀이)
+# 두번째 풀이
+    함수화, 전치행렬 간단히
 '''
+
+
+def my_sort(r_size, c_size, category):
+    max_row_size = 0
+    for i in range(r_size):
+        count = [0] * 101 # 몇개있는지 세줄거임
+        for j in range(c_size):
+            if category == "R":
+                if grid[i][j] == 0:
+                    continue
+                count[grid[i][j]] += 1
+            elif category == "C": 
+                if grid[j][i] == 0: # 0인애들 빼고
+                    continue
+                count[grid[j][i]] += 1 # 갯수 세거라.
+        sort_list = []
+        for i in range(101):
+            if count[i] > 0: # 정렬!
+                sort_list.append((i, count[i]))
+        sort_list.sort(key=lambda x: (x[1], x[0]))  # 정렬 !
+        max_row_size = max(len(sort_list) * 2, max_row_size)
+        tmp = []
+        for idx, cnt in sort_list:
+            tmp.extend([idx, cnt])
+        new_grid.append(tmp)
+    # 2. 최대 크기 기준으로 맞춰주기
+    for row in new_grid:
+        if len(row) < max_row_size:
+            row += [0] * (max_row_size - len(row))
+
 
 er, ec, target = map(int, input().split())
 er -= 1
 ec -= 1
 ans = -1
 grid = [list(map(int, input().split())) for i in range(3)]
+
 for time in range(101):
 
-    # 4. 목표 달성 검사
+    # 0. 목표 달성 검사
     if 0 <= er < len(grid) and 0 <= ec < len(grid[0]) and grid[er][ec] == target:
         ans = time
         break
 
     # 1. 크기에 따른 R,C 연산
-    r_size = len(grid)
-    c_size = len(grid[0])
+    r_size, c_size = len(grid), len(grid[0])
+    new_grid = []
     if r_size >= c_size:
-        new_grid = []
-        max_row_size = 0
-        for i in range(r_size):
-            count = [0] * 101
-            for j in range(c_size):
-                if grid[i][j] == 0:
-                    continue
-                count[grid[i][j]] += 1
-            sort_list = []
-            for i in range(101):
-                if count[i] > 0:
-                    sort_list.append((i, count[i]))
-            sort_list.sort(key=lambda x: (x[1], x[0]))
-            tmp = []
-            for count_num, idx in sort_list:
-                tmp.append(count_num)
-                tmp.append(idx)
-            max_row_size = max(max_row_size, len(tmp))
-            new_grid.append(tmp)
-        # 2. 최대 크기 기준으로 맞춰주기
-        for row in new_grid:
-            if len(row) < max_row_size:
-                row += [0] * (max_row_size - len(row))
+        my_sort(r_size, c_size, "R")
     else:
-        new_grid = []
-        max_col_size = 0
-        for j in range(c_size):
-            count = [0] * 101
-            for i in range(r_size):
-                if grid[i][j] == 0:
-                    continue
-                count[grid[i][j]] += 1
-            sort_list = []
-            for i in range(101):
-                if count[i] > 0:
-                    sort_list.append((i, count[i]))
-            sort_list.sort(key=lambda x: (x[1], x[0]))
-            tmp = []
-            for count_num, idx in sort_list:
-                tmp.append(count_num)
-                tmp.append(idx)
-            max_col_size = max(max_col_size, len(tmp))
-            new_grid.append(tmp)
-        # 2. 최대 크기 기준으로 맞춰주기
-        for row in new_grid:
-            if len(row) < max_col_size:
-                row += [0] * (max_col_size - len(row))
-        # 전치.
-        new_grid2 = [[0] * len(new_grid) for i in range(len(new_grid[0]))]
-        for i in range(len(new_grid)):
-            for j in range(len(new_grid[0])):
-                new_grid2[j][i] = new_grid[i][j]
-        new_grid = new_grid2
+        my_sort(c_size, r_size, "C")
+        new_grid = list(zip(*new_grid))  # 전치 필요
 
-    # 3. 행 or 열 크기가 100 넘어가면 버려주기. # 해당만 하면 실행하게 바꾸기
-    r_size = len(new_grid)
-    c_size = len(new_grid[0])
+    # 3. 행 or 열 크기가 100 넘어가면 버려주기.
+    r_size, c_size = len(new_grid), len(new_grid[0])
     is_small_need = False
     if r_size > 100:
         is_small_need = True
@@ -103,12 +68,7 @@ for time in range(101):
         c_size = 100
 
     if is_small_need:
-
-        new_grid2 = [[0] * c_size for i in range(r_size)]
-        for i in range(r_size):
-            for j in range(c_size):
-                new_grid2[i][j] = new_grid[i][j]
-        grid = new_grid2
+        grid = [_[:100] for _ in new_grid[:100]]
     else:
         grid = new_grid
 
