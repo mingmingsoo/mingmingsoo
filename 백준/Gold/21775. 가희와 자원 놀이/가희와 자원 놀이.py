a0b8450e99c9a59ef12bd9f9b4dd0b57ea9ca3ec
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-06 08:21:16 +0900

    [Gold IV] Title: 가희와 자원 놀이, Time: 748 ms, Memory: 293076 KB -BaekjoonHub

--- 백준/Gold/21775. 가희와 자원 놀이/가희와 자원 놀이.py
+++ 백준/Gold/21775. 가희와 자원 놀이/가희와 자원 놀이.py
@@ -1,5 +1,8 @@
 '''
-
+# 제출횟수: 4회
+# 메모리: 293088 KB
+# 시간: 792 ms
+# 헷 pypy 시간 1등 꺄
 
 문제설명
     T개 연산카드, 1 부터 2×109 이하의 자연수 자원 카드
@@ -10,8 +13,8 @@
             만약 자원크다그 누군가의 공간에 있으면 카드를 버리지 않고 재사용
         3. release n: 자연수 n이 적힌 자원 카드를 공용 공간에 반납
 
-누가 카드를 가지고있냐가 아니라
-이 카드를 누가 가지고있냐임
+    누가 무슨 카드를 가지고있냐가 아니라
+    이 카드를 누가 가지고있냐임!!!!!!!!!!!!!!!!!!!!!!!!
 '''
 import sys
 from collections import deque
@@ -22,7 +25,7 @@ who_num = list(map(lambda x: int(x) - 1, input().split()))
 private_card = dict()
 id_list = deque()
 order_list = deque()
-have_aquare = [0] * n
+have_acquire = [0] * n
 ans = []
 for game in range(game_num):
     tmp = list(input().split())
@@ -41,18 +44,18 @@ for game in range(game_num):
     id = id_list.popleft()
     order, card = order_list.popleft()
     # 어떤 카드가 들어오든 acquire 가 있으면 그거 먼저 처리해야함.
-    if have_aquare[who]:
-        origin_id, origin_card = have_aquare[who]
+    if have_acquire[who]:
+        origin_id, origin_card = have_acquire[who]
         order_list.appendleft((order, card))
         id_list.appendleft(id)
         if origin_card not in private_card:
             # 내가 가질 수 있으면
             private_card[origin_card] = who
             ans.append(origin_id)
-            have_aquare[who] = 0
+            have_acquire[who] = 0
         else:
             ans.append(origin_id)
-            have_aquare[who] = (origin_id, origin_card)
+            have_acquire[who] = (origin_id, origin_card)
     else:
         if order == "next":
             ans.append(id)
@@ -65,9 +68,9 @@ for game in range(game_num):
                 ans.append(id)
             else:
                 ans.append(id)
-                have_aquare[who] = (id, card)
+                have_acquire[who] = (id, card)
         else:
             private_card.pop(card)
             ans.append(id)
-for x in ans:
-    print(x)
+
+print('\n'.join(map(str,ans)))
