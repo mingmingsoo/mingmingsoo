380bb88911ec38f778020a88503ba5e7f0cc7fc5
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-06 16:30:44 +0900

    [Gold IV] Title: 인구 이동, Time: 1164 ms, Memory: 128844 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,4 +1,8 @@
 '''
+# 다섯번째 풀이
+    만약 인구이동이 많다면(최대 이천번뿐이긴 하지만......)
+    bfs호출 횟수가 많아질 것이라고 생각돼서
+    함수화 하지 않아보겠음
 # 네번째 풀이
     new_grid를 안쓰고
     visited를 활용해보겠서요
@@ -28,36 +32,6 @@ time = 0
 row = [-1, 1, 0, 0]
 col = [0, 0, 1, -1]
 
-
-def bfs(r, c):
-    global is_exit
-    q = deque([(r, c)])
-    cnt = 0
-    location = deque()
-    summ = 0
-    while q:
-        r, c = q.popleft()
-
-        location.append((r, c))
-        summ += grid[r][c]
-        cnt += 1
-        for k in range(4):
-            nr = r + row[k]
-            nc = c + col[k]
-            if not (0 <= nr < n and 0 <= nc < n) or visited[nr][nc]:
-                continue
-            if small <= abs(grid[r][c] - grid[nr][nc]) <= big:
-                visited[nr][nc] = True
-                q.append((nr, nc))
-    if cnt > 1:
-        is_exit = False
-    ele_num = summ // cnt
-
-    while cnt > 1 and location:
-        r, c = location.popleft()
-        grid[r][c] = ele_num
-
-
 while True:
 
     visited = [[False] * n for i in range(n)]
@@ -66,7 +40,31 @@ while True:
         for j in range(n):
             if not visited[i][j]:
                 visited[i][j] = True
-                bfs(i, j)
+                q = deque([(i, j)])
+                cnt = 0
+                location = deque()
+                summ = 0
+                while q:
+                    r, c = q.popleft()
+
+                    location.append((r, c))
+                    summ += grid[r][c]
+                    cnt += 1
+                    for k in range(4):
+                        nr = r + row[k]
+                        nc = c + col[k]
+                        if not (0 <= nr < n and 0 <= nc < n) or visited[nr][nc]:
+                            continue
+                        if small <= abs(grid[r][c] - grid[nr][nc]) <= big:
+                            visited[nr][nc] = True
+                            q.append((nr, nc))
+                if cnt > 1:
+                    is_exit = False
+                ele_num = summ // cnt
+
+                while cnt > 1 and location:
+                    r, c = location.popleft()
+                    grid[r][c] = ele_num
 
     if is_exit:
         break
