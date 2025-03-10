6828d99f791c44434140e5951736f2798159bb37
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-11 08:55:59 +0900

    [Gold III] Title: 사다리 조작, Time: 2248 ms, Memory: 137688 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.py
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -1,70 +1,28 @@
 '''
-문제 설명
-    사다리 정보가 주어졌을 때
-    모든 i 번이 i번으로 갈 수 있게끔
-    최소한의 가로 사다리를 놓아라
-
-입력
-    세로선 갯수(열), 가로선 갯수, 가로선을 놓을 수 있는 H(행)
-    기존에 있는 가로선
-        a, b(a와 a+1 세로선에 가로 b번째 위치)
-
-구상
-    연결된 가로가 있으면 무조건 가고
-    없으면 세로로감
-    조합 인데 행 위치를 담아줘야 두개 이상 연결이 안된다
-
-시간복잡도
-    300개중에 최대 3개 고를 수 있음(조합)
-    print((300*299*298)//(3*2)) = 4455100
-    여기다 가로길이만큼 탐색하므로  4455100 * 30 * 10 = 1_336_530_000 = 13초인데
-    하나라도 찾으면 튀게만들어서.. 괜찮을까 싶었다
-
-넣어본 테케
-    10 5 30
-    1 1
-    3 2
-    2 3
-    5 1
-    5 4
+# 세번째 풀이 사다리 조건 확인하는 while문 간단히.
 
 '''
 
 
+#################################################################
+# 두번째 풀이 -> 가로 연결 2개 되는 애들은 애초에 안담음
+# 첫번째 풀이와 100ms 시간차이
 def isOk():
-
     for j in range(0, m, 2):
         r, c = 0, j
-        while True:
-            # 만약 다 내려왔는데 c가 j 가 아니면 return False
-            if r == n:
-                if c != j:
-                    return False
-                else:
-                    break # 맞다면 다음 j 탐색
+        while r < n:
             # 오른쪽이나 왼쪽에 사다리 있으면 무조건 간다.
-            nr = r + row[0]
-            nc = c + col[0]
-            nr2 = r + row[1]
-            nc2 = c + col[1]
-            if 0 <= nc < m and grid[nr][nc] == 1:
-                while 0 <= nc < m and grid[nr][nc] == 1:
-                    r = nr
-                    c = nc
-                    nr = r + row[0]
-                    nc = c + col[0]
-                r += 1
-            elif 0 <= nc2 < m and grid[nr2][nc2] == 1:
-                while 0 <= nc2 < m and grid[nr2][nc2] == 1:
-                    r = nr2
-                    c = nc2
-                    nr2 = r + row[1]
-                    nc2 = c + col[1]
-                r += 1
-            else:
-                # 아니면 내려만 간다.
-                r += 1
-    return True # 여기까지 왔으면 조건 만족했음
+            if c > 0 and grid[r][c - 1] == 1:
+                while c > 0 and grid[r][c - 1] == 1:
+                    c -= 1
+            elif c < m - 1 and grid[r][c + 1] == 1:
+                while c < m - 1 and grid[r][c + 1] == 1:
+                    c += 1
+            r += 1
+        if c != j:
+            return False
+    return True  # 여기까지 왔으면 조건 만족했음
+
 
 def combi(sidx, idx, M):
     global find
@@ -84,11 +42,11 @@ def combi(sidx, idx, M):
         return
 
     # 가로사다리 2개 연결되는지 검증
-    if sidx>0:
-        pr,pc = sel[sidx-1] # 이전에 선택한 좌표
-        r,c = arr[idx] # 내가 선택하려는 좌표
-        if pr == r and c-pc == 2:
-            combi(sidx, idx + 1, M) # 가로 2개네 ? 다음꺼 골라
+    if sidx > 0:
+        pr, pc = sel[sidx - 1]  # 이전에 선택한 좌표
+        r, c = arr[idx]  # 내가 선택하려는 좌표
+        if pr == r and c - pc == 2:
+            combi(sidx, idx + 1, M)  # 가로 2개네 ? 다음꺼 골라
             return
 
     sel[sidx] = arr[idx]
