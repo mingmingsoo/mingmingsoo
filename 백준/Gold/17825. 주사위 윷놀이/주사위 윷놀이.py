aa330cdf2170368a01a9de3328b6ea3ee21cdf18
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-12 20:05:00 +0900

    [Gold II] Title: 주사위 윷놀이, Time: 140 ms, Memory: 111496 KB -BaekjoonHub

--- 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.py
+++ 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.py
@@ -1,91 +1,59 @@
-'''
-틀린이유
-    .... 가려는 위치에 말이 있으면 말이 대기하는 건 줄 알았ㅇㅁ..............
-    .....ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ
-    ㅜㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ
-    ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ
-    ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ
+#       0   1   2   3   4         5    6   7   8   9
+adj = [[1], [2], [3], [4], [5], [6, 11], [7], [8], [9], [10],
+#      10       11  12    13  14   15   16    17   18   19
+       [16, 14], [12], [13], [28], [15], [28], [17], [18], [19], [20],
+#      20      21   22   23   24   25   26   27   28   29
+       [21, 25], [22], [23], [24], [31], [26], [27], [28], [29], [30],
+#      30   31(도착점)
+       [31], [32]]
 
-문제설명
-    4개의 말이 있을 때 받을 수 있는 최대 점수는?
+score_info = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18,
+              20, 13, 16, 19, 22, 24, 22, 24, 26, 28,
+              30, 32, 34, 36, 38, 28, 27, 26, 25, 30, 35, 40, 0]
 
-구상
-    1,2,3,4 중복순열
-
-반례
-5 5 5 5 5 5 5 5 5 5
-정답 130
-
-
-'''
-'''
-문제설명
-    4개의 말이 있을 때 받을 수 있는 최대 점수는?
-구상
-    1,2,3,4 중복순열
-반례
-5 5 5 5 5 5 5 5 5 5
-정답 130
-
-25 처리해주는 게 필요함.
-'''
-cube = list(map(int, input().split()))  # 짬푸할 칸.
-sel = [0] * 10
+state = [0, 0, 0, 0]  # 말 위치 -1 이면 죽은거
+dice = list(map(int, input().split()))
 ans = 0
-score = [[2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40],
-         [10, 13, 16, 19, 25],
-         [20, 22, 24, 25],
-         [30, 28, 27, 26, 25],
-         [25, 30, 35, 40]]
 
 
-def perm(idx):
+def btk(idx, score):
     global ans
-    if idx == 10:
-        ele_score = 0
-        state = [(0, -1) for i in range(4)]
-        for i in range(10):
-            if (10 - i) * 40 + ele_score < ans:
-                return
-            horse = sel[i] - 1
-            dice = cube[i]
-            if (state[horse][0], state[horse][1]) == (-1, -1):
-                return
-            nr, nc = state[horse][0], state[horse][1] + dice
-
-            if (nr, nc) == (0, 4):  # 10
-                (nr, nc) = (1, 0)  # 위치 바꿔줌
-            elif (nr, nc) == (0, 9):  # 20
-                (nr, nc) = (2, 0)
-            elif (nr, nc) == (0, 14):  # 30
-                (nr, nc) = (3, 0)
-            elif (nr, nc) == (0, 19):  # 40
-                (nr, nc) = (3, 7)
-            if 0 < nr < 4 and nc >= len(score[nr]) - 1:
-                nc = nc - len(score[nr]) + 1
-                nr = 4
-
-            if nr == 0 and nc > 19:
-                state[horse] = (-1, -1)
-                continue
-            elif nr == 4 and nc > 3:
-                state[horse] = (-1, -1)
-                continue
-
-            for j in range(4):
-                if j == horse:
-                    continue
-                hr, hc = state[j]
-                if (hr, hc) == (nr, nc):
-                    return
-            state[horse] = (nr, nc)
-            ele_score += score[nr][nc]
-        ans = max(ans, ele_score)
+    if (10 - idx) * 40 + score <= ans:
         return
-    for i in range(1, 5):
-        sel[idx] = i
-        perm(idx + 1)
+    if idx == 10:
+        ans = max(ans, score)
+        return
+    for i in range(4):
+        if state[i] == -1:
+            continue  # 죽은 말이면 넘어가!
+        cur = state[i]
+        go = dice[idx]
+
+        # 일단 한번 이동인데 파란색이면 파란색으로 갈 수 있게
+        next = adj[cur][-1]
+        if next >= 32:
+            next = -1
+        else:
+            # 나머지 칸 이동
+            for j in range(go - 1):
+                next = adj[next][0]
+                if next >= 32:
+                    next = -1
+                    break
+        possible = True
+        for j in range(4):
+            if i == j:
+                continue
+            if state[j] == next and next != - 1:
+                possible = False
+                break
+        if possible:
+            state[i] = next
+            btk(idx + 1, score + score_info[next])
+            state[i] = cur
+        else:
+            continue
 
 
-perm(0)
+btk(0, 0)  # 몇번째 다이스인지, 점수
 print(ans)
