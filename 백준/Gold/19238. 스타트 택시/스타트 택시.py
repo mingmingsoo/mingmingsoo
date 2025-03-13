d9ddd1cf04b8d3fbb3323539b4a1bbaddb7b232b
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-13 16:14:37 +0900

    [Gold II] Title: 스타트 택시, Time: 208 ms, Memory: 115632 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.py
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.py
@@ -1,47 +1,4 @@
-'''
-문제설명
-    1. 최단경로에 있는 승객 태우는데 행, 열 우선순위
-    2. 승객태우고~ 도착까지 사용된 연료*2 를 목적지에 도달하면 채워짐(갈 때가 아니라)
-    3. 이동 중 바닥나면 실패 업무끝 -1
-    4. 모든 손님 이동할수 없으면 -1
-구상
-    bfs 힙큐 쓰고 다 돌아도 괜찮을듯?
-    2차원 배열에 시작위치에만 손님 표시하고(인덱스 넘버링)
-    인덱스와 도착위치를 담은 딕셔너리 생성
-입력
-    맵 크기, 손님 수, 처음 연료
-    맵 정보
-    택시 시작위치
-    승객 위치
 
-반례
-6 1 15
-0 0 1 0 0 0
-1 1 1 0 0 0
-0 0 0 0 0 0
-0 0 0 0 0 0
-0 0 0 0 0 0
-0 0 0 0 0 0
-5 5
-5 5 0 0
-정답 -1
-
-
-6 1 15
-0 0 1 0 0 0
-1 1 1 0 0 0
-0 0 0 0 0 0
-0 0 0 0 0 0
-0 0 0 0 0 0
-0 0 0 0 0 0
-5 5
-5 5 3 3
-19
-택시 시작 위치가 승객일 때
-
-
-
-'''
 import heapq
 from collections import deque
 
@@ -50,13 +7,13 @@ grid = [list(map(int, input().split())) for i in range(n)]
 for i in range(n):
     for j in range(n):
         if grid[i][j] == 1:
-            grid[i][j] = -1
+            grid[i][j] = -1 # 택시 넘버링해줄거라 못가는 곳은 -1처리
 r, c = map(lambda x: int(x) - 1, input().split())
 end_info = {}
 for mm in range(m):
     sr, sc, er, ec = map(lambda x: int(x) - 1, input().split())
-    end_info[mm + 1] = (er, ec)
-    grid[sr][sc] = mm + 1
+    end_info[mm + 1] = (er, ec) # 도착지 정보 기록해두기
+    grid[sr][sc] = mm + 1 # 맵에 넘버링 해주기
 
 row = [-1, 1, 0, 0]
 col = [0, 0, 1, -1]
@@ -64,58 +21,62 @@ col = [0, 0, 1, -1]
 ans = -1
 for mm in range(m):
     used_power, next_r, next_c, next_numbering = -1, -1, -1, -1
-    # 혹시 내 위치가 손님 위치..?
-    if grid[r][c] !=0 and grid[r][c] != -1:
+    # [1-1] 혹시 내 위치가 손님 위치..?
+    if grid[r][c] != 0 and grid[r][c] != -1:
         used_power = 0
         next_r = r
         next_c = c
         next_numbering = grid[r][c]
+    # [1-2] 아니라면 최단경로 손님 찾기(bfs) -> 힙큐에 후보 담아줌
     else:
-        # 최단경로 손님 찾기(bfs) -> 힙큐 # 연료 검사
         user = []
         visited = [[False] * n for i in range(n)]
         visited[r][c] = True
         q = deque([(r, c, 0)])
         used_power, next_r, next_c, next_numbering = -1, -1, -1, -1
         while q:
-            r, c, dist = q.popleft()
-            for k in range(4):
-                nr = r + row[k]
-                nc = c + col[k]
-                if not (0 <= nr < n and 0 <= nc < n) or visited[nr][nc] or grid[nr][nc] == -1:
-                    continue
-                if grid[nr][nc] == 0:
-                    visited[nr][nc] = True
-                    q.append((nr, nc, dist + 1))
-                elif grid[nr][nc] != 0:
-                    visited[nr][nc] = True
-                    q.append((nr, nc, dist + 1))
-                    heapq.heappush(user, (dist + 1, nr, nc, grid[nr][nc]))
-        if user:
-            used_power, next_r, next_c, next_numbering = heapq.heappop(user)
-        else:
-            power = -1
-            break
-        if used_power > power:
-            power = -1
-            break
-    grid[next_r][next_c] = 0
-    power -= used_power
-    # print(used_power, power)
-    # 손님 데려다주기(bfs) -> # 얘도 연료 검사
-    er, ec = end_info[next_numbering]
-    r, c = next_r, next_c
+            q_size = len(q)
+            for qs in range(q_size):
+                r, c, dist = q.popleft()
+                for k in range(4):
+                    nr = r + row[k]
+                    nc = c + col[k]
+                    if not (0 <= nr < n and 0 <= nc < n) or visited[nr][nc] or grid[nr][nc] == -1:
+                        continue
+                    if grid[nr][nc] == 0:
+                        visited[nr][nc] = True
+                        q.append((nr, nc, dist + 1))
+                    elif grid[nr][nc] != 0:
+                        visited[nr][nc] = True
+                        q.append((nr, nc, dist + 1))
+                        heapq.heappush(user, (dist + 1, nr, nc, grid[nr][nc]))
+            if user: # 손님 태울 수 있었으면 데리러 간다
+                used_power, next_r, next_c, next_numbering = heapq.heappop(user)
+                break
+    if next_r == -1:
+        power = -1
+        break
+    if used_power > power: # 근데 가는길에 연료 바닥났었으면 불가함 종료
+        power = -1
+        break
+
+    grid[next_r][next_c] = 0 # 태웠으니까 맵에 0처리
+    power -= used_power # 연료 소모 반영
+    r, c = next_r, next_c # 택시 위치 반영
+
+    # [2] 손님 데려다주기(bfs)
+    er, ec = end_info[next_numbering] # 목적지
     used_power = 0
 
     visited = [[False] * n for i in range(n)]
     visited[r][c] = True
     q = deque([(r, c, 0)])
-    end = False
+    end = False # 못데려다줬으면 납치이므로 -1 처리 해줄거임 그래서 필요한 플래그
     while q:
         r, c, dist = q.popleft()
         if r == er and c == ec:
             used_power = dist
-            end = True
+            end = True # 데려다줬습니다!
             break
         for k in range(4):
             nr = r + row[k]
@@ -124,14 +85,15 @@ for mm in range(m):
                 continue
             visited[nr][nc] = True
             q.append((nr, nc, dist + 1))
-    if used_power > power:
+    if used_power > power: # 연료바닥났으면.. 미션 실패
         power = -1
         break
-    if not end:
+    if not end: # 납치 했어도 미션 실패 ..
         power = -1
         break
+    # 연료 반영
     power -= used_power
     power += used_power * 2
-    # print(used_power, power)
+    # 택시 위치 반영
     r, c = er, ec
 print(power)
