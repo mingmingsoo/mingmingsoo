8791f688e9b40f1b07dc0728b20a258abceef317
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-17 19:15:02 +0900

    [Gold II] Title: 가스관, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/2931. 가스관/가스관.py
+++ 백준/Gold/2931. 가스관/가스관.py
@@ -6,24 +6,21 @@
     .을 만나면 경우수를 계산
     그다음에 dfs 연결 확인해줘야함
 
+
+
+    아님 ! 그냥 ,, 방향에 따라 따져주면됨
+    M과 Z는 중요하지 않음
+
 '''
 n, m = map(int, input().split())
 grid = [list(input()) for i in range(n)]
-sr = sc = er = ec = -1
-for i in range(n):
-    for j in range(m):
-        if grid[i][j] == "M":
-            sr, sc = i, j
-        elif grid[i][j] == "Z":
-            er, ec = i, j
 row = [-1, 0, 1, 0]
 col = [0, 1, 0, -1]
 target_r, target_c, target_shape = -1, -1, ""
 
-dirs = {"|": (0, 2), "-": (1, 3), "+": (0, 1, 2, 3), "1": (1, 2), "2": (0, 1),
-        "3": (0, 3), "4": (2, 3)}
-dirs_reverse = {(0, 2): "|", (1, 3): "-", (0, 1, 2, 3): "+", (1, 2): "1", (0, 1): "2",
-                (0, 3): "3", (2, 3): "4"}
+dirs = {"|": (0, 2), "-": (1, 3), "+": (0, 1, 2, 3), "1": (1, 2), "2": (0, 1), "3": (0, 3), "4": (2, 3)}
+dirs_reverse = {(0, 2): "|", (1, 3): "-", (0, 1, 2, 3): "+", (1, 2): "1", (0, 1): "2", (0, 3): "3", (2, 3): "4"}
+connect = {0: ("|", "+", "1", "4"), 1: ("-", "+", "3", "4"), 2: ("|", "+", "2", "3"), 3: ("-", "+", "1", "2")}
 
 
 def find_target():
@@ -39,21 +36,13 @@ def find_target():
                     if grid[nr][nc] == ".":
                         target_r, target_c = nr, nc
                         shape = []
-                        if 0 <= target_r + row[0] < n:
-                            if grid[target_r + row[0]][target_c] in ("|", "+", "1", "4"):
-                                shape.append(0)
-                        if 0 <= target_r + row[2] < n:
-                            if grid[target_r + row[2]][target_c] in ("|", "+", "2", "3"):
-                                shape.append(2)
-
-                        if 0 <= target_c + col[3] < m:
-                            if grid[target_r][target_c + col[3]] in ("-", "+", "1", "2"):
-                                shape.append(3)
-
-                        if 0 <= target_c + col[1] < m:
-                            if grid[target_r][target_c + col[1]] in ("-", "+", "3", "4"):
-                                shape.append(1)
-                        shape.sort()
+                        for k in range(4):
+                            ntr = target_r + row[k]
+                            ntc = target_c + col[k]
+                            if not (0 <= ntr < n and 0 <= ntc < m):
+                                continue
+                            if grid[ntr][ntc] in connect[k]:
+                                shape.append(k)
                         target_shape = dirs_reverse[tuple(shape)]
                         return
 
