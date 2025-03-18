c39b22570204c96b93b8e0b6bfd896354b3284da
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-03-18 20:00:30 +0900

    [Gold IV] Title: 불, Time: 628 ms, Memory: 229576 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.py
+++ 백준/Gold/5427. 불/불.py
@@ -1,83 +1,64 @@
+from collections import deque
+
 T = int(input())
 for tc in range(T):
     '''
-    문제
-        지훈이가 불에 타기전에 미로를 탈출할 수 있는가?
-        불은 4방향으로 확산
-    입력
-        R,C
-        맵 정보
-
-        #: 벽
-        .: 지나갈 수 있는 공간
-        J: 지훈이의 미로에서의 초기위치 (지나갈 수 있는 공간)
-        F: 불이 난 공간
-    출력
-        미로를 탈출할 수 있으면 빠른 탈출시간
-        없으면 IMPOSSIBLE
-    구상
-        지훈이 q와 불 q를 따로 관리.
-        지훈이 먼저 움직인다.
-        지훈이가 범위 밖으로 벗어날 수 있으면 q는 종료
-
-    헷갈리는 것,,, 불이 지훈이를 덮칠 수 있는가?
+    불!!!!!!!!!!!!!!!!
+    상근이가 이동하고 불이 퍼진다
+    아니다.. 불이 먼저 퍼진다..
+    테케 1번을 잘못 봤다.
     '''
-    from collections import deque
 
     m, n = map(int, input().split())
     grid = [list(input()) for i in range(n)]
-
     player_q = deque()
     fire_q = deque()
-    visited = [[False] * m for i in range(n)]  # 지훈이 visited
-
+    visited = [[False] * m for i in range(n)]
     for i in range(n):
         for j in range(m):
             if grid[i][j] == "@":
-                player_q.append((i, j, 1))
+                player_q.append((i, j, 0))
                 visited[i][j] = True
                 grid[i][j] = "."
             elif grid[i][j] == "*":
                 fire_q.append((i, j))
     ans = "IMPOSSIBLE"
+    row = [-1, 1, 0, 0]
+    col = [0, 0, 1, -1]
 
 
     def bfs():
         global ans
-        row = [-1, 1, 0, 0]
-        col = [0, 0, 1, -1]
 
         while player_q:
             if fire_q:
                 fq_size = len(fire_q)
-                for s in range(fq_size):
+                for fq in range(fq_size):
                     fr, fc = fire_q.popleft()
                     for k in range(4):
-                        fnr = fr + row[k]
-                        fnc = fc + col[k]
-                        if not (0 <= fnr < n and 0 <= fnc < m):
+                        nr = fr + row[k]
+                        nc = fc + col[k]
+                        if not (0 <= nr < n and 0 <= nc < m):
                             continue
-                        if grid[fnr][fnc] == ".":
-                            grid[fnr][fnc] = "*"
-                            fire_q.append((fnr, fnc))
+                        if grid[nr][nc] == ".":
+                            grid[nr][nc] = "*"
+                            fire_q.append((nr, nc))
 
             pq_size = len(player_q)
-            for ps in range(pq_size):
+            for pq in range(pq_size):
                 r, c, time = player_q.popleft()
-
                 for k in range(4):
                     nr = r + row[k]
                     nc = c + col[k]
-                    if not (0 <= nr < n and 0 <= nc < m):
-                        ans = time
-                        return
-                    if not visited[nr][nc] and grid[nr][nc] == ".":
-                        visited[nr][nc] = True
-                        player_q.append((nr, nc, time + 1))
 
+                    if not (0 <= nr < n and 0 <= nc < m):
+                        ans = time + 1
+                        return
+                    if grid[nr][nc] != "." or visited[nr][nc]:
+                        continue
+                    player_q.append((nr, nc, time + 1))
+                    visited[nr][nc] = True
 
 
     bfs()
     print(ans)
-    # for _ in grid:
-    #     print(_)
