38cee6cd73fbda0bb188768300f7ffcbad7be8e7
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-04-03 09:34:05 +0900

    [Unrated] Title: [모의 SW 역량테스트] 핀볼 게임, Time: 1,578 ms, Memory: 84,916 KB -BaekjoonHub

--- SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.py
+++ SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.py
@@ -1,123 +1,93 @@
 T = int(input())
 for tc in range(T):
-    '''
-    * 두번째 풀이
-    * 벽 또는 정사각을 만나면 무조건 되돌아가게 되있음. 점수 = 점수*2+1이됨
-    1. 0인 곳에서 4가지 방향으로 모든 점수 계산
-    2. 웜홀의 정보를 담는 배열 필요
-    '''
-    n = int(input())
+    from collections import defaultdict
 
-    grid = [list(map(int, input().split())) for i in range(n)]
+    n = int(input())
+    grid = [[5] * (n + 2)] + [[5] + list(map(int, input().split())) + [5] for i in range(n)] + [[5] * (n + 2)]
+    n += 2
     ans = 0
 
-    # 웜홀 정보를 담는 배열
-    holls = [[] for i in range(5)]
-
-    for i in range(n):
-        for j in range(n):
-            if (grid[i][j] >= 6):
-                holls[grid[i][j] - 6].append([i, j])
-    # for row in holls:
-    #     print(row)
+    white_holl = defaultdict(list)
+    for i in range(1, n - 1):
+        for j in range(1, n - 1):
+            if 6 <= grid[i][j] <= 10:
+                white_holl[grid[i][j]].append((i, j))
 
     row = [-1, 0, 1, 0]
-    col = [0, -1, 0, 1]
+    col = [0, 1, 0, -1]
 
 
-    def startGame(r, c, d):
-        ping = 0
-        originR = r
-        originC = c
-
-        # 지금 위치가 벽인가 +1
-        # 이동하는 곳이 1~5이면 +1
+    def game(sr, sc, d):
+        global ans
+        r, c = sr, sc
+        score = 0
 
         while True:
-            nextR = r + row[d]
-            nextC = c + col[d]
 
-            # 벽 검사
-            if (nextR < 0 or nextR >= n or nextC < 0 or nextC >= n):
-                # 벽에 부닥쳤음
-                ping = ping* 2 +1
+            nr = r + row[d]
+            nc = c + col[d]
+
+            # 종료 조건
+            if grid[nr][nc] == -1 or (nr, nc) == (sr, sc):
+                ans = max(ans, score)
+
                 break
 
-            # 블록 검사
-            if (1 <= grid[nextR][nextC] <= 5):
-                ping += 1
-                if (d == 0):
-                    # 0(상)일때 2를 만나면 3이 되고 3을 만나면 1이됨
-                    if (grid[nextR][nextC] == 2):
-                        d = 3
-                    elif (grid[nextR][nextC] == 3):
-                        d = 1
-                    else:  # 나머지는 방향 180도 전환
-                        ping = (ping-1)* 2 +1
-                        break
-                elif (d == 1):
-                    # 1(좌)일때 1를 만나면 0이 되고 2을 만나면 2이됨
-                    if (grid[nextR][nextC] == 1):
-                        d = 0
-                    elif (grid[nextR][nextC] == 2):
-                        d = 2
-                    else:  # 나머지는 방향 180도 전환
-                        ping = (ping-1)* 2 +1
-                        break
-                elif (d == 2):
-                    # 2(하)일때 1를 만나면 3이 되고 4을 만나면 1이됨
-                    if (grid[nextR][nextC] == 1):
-                        d = 3
-                    elif (grid[nextR][nextC] == 4):
-                        d = 1
-                    else:  # 나머지는 방향 180도 전환
-                        ping = (ping - 1) * 2 + 1
-                        break
-                elif (d == 3):
-                    # 3(우)일때 4를 만나면 0이 되고 3을 만나면 3이됨
-                    if (grid[nextR][nextC] == 4):
-                        d = 0
-                    elif (grid[nextR][nextC] == 3):
-                        d = 2
-                    else:  # 나머지는 방향 180도 전환
-                        ping = (ping-1)* 2 +1
-                        break
+            if d == 0:
+                if grid[nr][nc] == 1 or grid[nr][nc] == 4 or grid[nr][nc] == 5:
+                    d = (d + 2) % 4
+                    score += 1
+                elif grid[nr][nc] == 2:
+                    d = 1
+                    score += 1
+                elif grid[nr][nc] == 3:
+                    d = 3
+                    score += 1
+            elif d == 1:
+                if grid[nr][nc] == 1 or grid[nr][nc] == 2 or grid[nr][nc] == 5:
+                    d = (d + 2) % 4
+                    score += 1
+                elif grid[nr][nc] == 3:
+                    d = 2
+                    score += 1
+                elif grid[nr][nc] == 4:
+                    d = 0
+                    score += 1
+            elif d == 2:
+                if grid[nr][nc] == 2 or grid[nr][nc] == 3 or grid[nr][nc] == 5:
+                    d = (d + 2) % 4
+                    score += 1
+                elif grid[nr][nc] == 1:
+                    d = 1
+                    score += 1
+                elif grid[nr][nc] == 4:
+                    d = 3
+                    score += 1
+            elif d == 3:
+                if grid[nr][nc] == 3 or grid[nr][nc] == 4 or grid[nr][nc] == 5:
+                    d = (d + 2) % 4
+                    score += 1
+                elif grid[nr][nc] == 1:
+                    d = 0
+                    score += 1
+                elif grid[nr][nc] == 2:
+                    d = 2
+                    score += 1
 
-            # 블랙홀 검사 : 게임 종료
-            elif (grid[nextR][nextC] == -1):
-                break
-
-            # 원래 위치로 돌아왔는지: 게임 종료
-            elif (nextR == originR and nextC == originC):
-                break
-
-            # 웜홀 검사
-            # 가려고 하는 위치가 웜홀과 하나라도 같으면..
-            elif (grid[nextR][nextC] >= 6):
-                idx = grid[nextR][nextC] - 6
-                if (nextR == holls[idx][0][0] and nextC == holls[idx][0][1]):
-                    nextR = holls[idx][1][0]
-                    nextC = holls[idx][1][1]
+            # 웜홀
+            if 6 <= grid[nr][nc] <= 10:
+                (wsr, wsc), (wer, wec) = white_holl[grid[nr][nc]]
+                if (nr, nc) == (wsr, wsc):
+                    nr, nc = wer, wec
                 else:
-                    nextR = holls[idx][0][0]
-                    nextC = holls[idx][0][1]
-
-            r = nextR
-            c = nextC
-        return ping
+                    nr, nc = wsr, wsc
+            r = nr
+            c = nc
 
 
-    for i in range(n):
-        for j in range(n):
-            if (grid[i][j] == 0):
-                # 좌표와 방향 d
-                # print("=========i: ", i, "j: ", j , "d: ", 0)
-                ans = max(startGame(i, j, 0), ans)  # 상
-                # print("=========i: ", i, "j: ", j , "d: ", 1)
-                ans = max(startGame(i, j, 1), ans)  # 좌
-                # print("=========i: ", i, "j: ", j , "d: ", 2)
-                ans = max(startGame(i, j, 2), ans)  # 하
-                # print("=========i: ", i, "j: ", j , "d: ", 3)
-                ans = max(startGame(i, j, 3), ans)  # 우
-
-    print(f"#{tc+1} {ans}")
+    for i in range(1, n - 1):
+        for j in range(1, n - 1):
+            if not grid[i][j]:
+                for k in range(4):
+                    game(i, j, k)
+    print(f"#{tc + 1} {ans}")
