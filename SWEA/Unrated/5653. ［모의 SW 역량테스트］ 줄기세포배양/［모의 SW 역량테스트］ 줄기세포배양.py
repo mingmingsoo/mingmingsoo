18a91ca27a1130f560caed3cd68dce06b433ac0e
mingmingsoo <163358979+mingmingsoo@users.noreply.github.com>
2025-04-09 09:48:37 +0900

    [Unrated] Title: [모의 SW 역량테스트] 줄기세포배양, Time: 844 ms, Memory: 216,064 KB -BaekjoonHub

--- SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.py
+++ SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.py
@@ -1,101 +1,92 @@
-import heapq
-from collections import deque
-
-'''
-우선순위큐 대신 리스트로 정렬해서 푼 풀이.
-시간복잡도 괜찮으면 이렇게 하는게 나을듯. -> 실제로 이게 더 빠름
-
-문제 설명
-1. 시작점을 기준으로 생명력에 따라 무한 증식함.
-2. 증식후에, 생명력만큼 살아있고 죽는 과정이 필요함.
--> 무한 증식은 bfs를 사용해 품
--> 죽는 과정은 증식한 좌표를 따로 다른 Q에 담아서 해결함.
-
-'''
-
-class Node: # 정렬을 위해 class 생성
-    def __init__(self, r, c, time, cur):
-        self.r = r
-        self.c = c
-        self.time = time
-        self.cur = cur
-
-    def __lt__(self, other): # 생명력이 큰 애들 우선임.
-        return self.time > other.time
-
-
 T = int(input())
+
+
+def make_q():
+    for i in range(N):
+        for j in range(N):
+            if grid[i][j] and grid[i][j] != -1:
+                if grid[i][j][0] > 0:
+                    grid[i][j][0] -= 1
+                elif grid[i][j][0] == 0:
+                    q.append((i, j, grid[i][j][2]))
+                    grid[i][j][1] += 1
+
+
+def myprint():
+    print("-------------------")
+    for i in range(N):
+        for j in range(N):
+            if grid[i][j] and grid[i][j] != -1:
+                print(grid[i][j][2], end=" ")
+            elif grid[i][j] == -1:
+                print("X", end=" ")
+            else:
+                print(0, end=" ")
+        print()
+
+
 for tc in range(T):
+    '''
+    문제 설명
+        t초후 줄기세포 갯수
+    입력
+        n,m, 시간 time
+    구상
+        맵 800*800 늘리기
+        그리고 400,400에 위치시키기
+    '''
+    from collections import deque
 
-    size = 700 # k가 최대 300이여서 600정도의 맵이 필요함.
-    n, m, k = map(int, input().split())
-    grid = [[0] * size for i in range(size)]
+    n, m, time = map(int, input().split())
+    tmp = [list(map(int, input().split())) for i in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if tmp[i][j]:
+                tmp[i][j] = [tmp[i][j], 0, tmp[i][j]]  # 0 번째가 0이되면 활성화 1번째가 2번째가 되면 쥬금
+    N = 700
+    # N = 20
+    grid = [[0] * N for i in range(N)]
 
-    # 중간 지점에 맵을 찍기위한 연산.
-    startX = size // 2 - n // 2
-    startY = size // 2 - m // 2
-    endX = size // 2 + n // 2
-    endY = size // 2 + m // 2
-    if (n % 2 == 1):
-        endX += 1
-    if (m % 2 == 1):
-        endY += 1
-    # 확장할 좌표를 담을 q
-    q = []
-    for i in range(startX, endX):
-        tmp = list(map(int, input().split()))
-        for j in range(startY, endY):
-            grid[i][j] = tmp[j - startY]
-            if (grid[i][j] > 0):
-                q.append(Node(i, j, grid[i][j], 0))
-                # 위치, 생명력, 진행 시간
+    for i in range(n):
+        for j in range(m):
+            if tmp[i][j]:
+                grid[i + N // 2][j + N // 2] = tmp[i][j]
+    # for _ in grid:
+    #     print(_)
     row = [-1, 1, 0, 0]
     col = [0, 0, 1, -1]
-    limit = 0
-    dieQ = [] # 죽어야 될 애들을 담을 q
-    while q:
-        if (limit >= k): # 시간이 다다랐으면 종료
-            break
-        qSize = len(q)  # q 초기 size 만큼만 진행되야함.
-        q.sort() # 생명력 순으로 정렬
-        for s in range(qSize):
-            node = q.pop(0)
-            r = node.r
-            c = node.c
-            time = node.time
-            curTime = node.cur
-            if (curTime < time): # 아직 비활성상태면 기다려야함
-                q.append(Node(r, c, time, curTime + 1))
-            elif (curTime == time):  # 활성화 될 수 있는 시간이 됐으면 확장 가능
-                dieQ.append(Node(r, c, time, 1)) # 대신 곧 죽을애들이라 dieQ에 넣어줌
-                # 확장하는 로직
-                for d in range(4):
-                    nr = r + row[d]
-                    nc = c + col[d]
-                    if (nr < 0 or nr >= size or nc < 0 or nc >= size):
-                        continue
-                    elif (grid[nr][nc] == 0): # 빈공간으로 확장 가능
-                        q.append(Node(nr, nc, time, 0))
-                        grid[nr][nc] = time
-                    elif (grid[nr][nc] == -1): # 죽은 공간으로는 확장 불가.
-                        continue
-        if dieQ: # 죽을 애들을 검사
-            dieQsize = len(dieQ) # 얘도 초기값만큼만 작동해야함.
-            for dieS in range(dieQsize):
-                node2 = dieQ.pop(0)
-                r, c, time, cur = node2.r, node2.c, node2.time, node2.cur
-                if (time == cur): # 죽을 때가 됐으면 죽어!
-                    grid[r][c] = -1
-                else: # 아니면 활성화 상태 유지.
-                    dieQ.append(Node(r, c, time, cur + 1))
 
-        limit += 1
-        # print("----------")
-        # for ele in grid:
-        #     print(" ".join(f"{num:2}" for num in ele))
-    sum = 0
-    for i in range(size):
-        for j in range(size):
-            if (grid[i][j] > 0):
-                sum += 1
-    print(f"#{tc + 1} {sum}")
+    for t in range(time):
+
+        q = deque()
+        # 1. 1 이상인 애들 0 빼주기, 0인 애들은 q 에 넣어주기
+        make_q()
+
+        # print(q)
+        # 2. q 인애들 bfs 돌려서 확산하기
+        die_set = set()
+        while q:
+            r, c, power = q.popleft()
+            if grid[r][c][1] == grid[r][c][2]:
+                die_set.add((r, c))
+            for k in range(4):
+                nr = r + row[k]
+                nc = c + col[k]
+                if not (0 <= nr < N and 0 <= nc < N) or grid[nr][nc] == -1:
+                    continue
+                if grid[nr][nc] == 0:
+                    grid[nr][nc] = [power, 0, power]
+                elif grid[nr][nc][0] == grid[nr][nc][2] and grid[nr][nc][2] < power:  # 내가 더 클때만 가능
+                    grid[nr][nc] = [power, 0, power]
+
+        # # 1. 죽은 애들 검사 -> 1번째 2번째 같은 애들 -1로 바꿔주기
+        for r, c in die_set:
+            grid[r][c] = -1
+        # myprint()
+    ans = 0
+    for i in range(N):
+        for j in range(N):
+            if grid[i][j] != 0 and grid[i][j] != -1:
+                ans += 1
+
+    print(f"#{tc + 1} {ans}")
